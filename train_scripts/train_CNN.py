c515bfa57a8155a757181d971b48dd617c96b854
Jan <uqdqa@student.kit.edu>
2018-10-19 19:53:24 +0200

    base script for cnn training

---
+++ train_scripts/train_CNN.py
@@ -0,0 +1,44 @@
+# global imports
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(filedir)
+sys.path.append(basedir)
+#import DRACO_Frameworks.CNN.data_frame as data_frame
+import DRACO_Frameworks.CNN.CNN as CNN
+
+
+inPath = "/storage/c/vanderlinden/DRACO-MLfoy/workdir/CNN_input_data/base_train_set"
+
+cnn = CNN.CNN(
+    in_path         = inPath,
+    save_path       = basedir+"/workdir/cnn_test",
+    class_label     = "class_label",
+    batch_size      = 256,
+    train_epochs    = 1,
+    optimizer       = "adam",
+    loss_function   = "categorical_crossentropy",
+    eval_metrics    = ["mean_squared_error", "acc"] )
+
+cnn.load_datasets()
+cnn.build_model()
+cnn.train_model()
+cnn.eval_model()
+
+# evaluate stuff
+cnn.print_classification_examples()
+cnn.print_classification_report()
+cnn.plot_metrics()
+cnn.plot_discriminators()
+cnn.plot_confusion_matrix()
+
+
+
+
+
+
+
