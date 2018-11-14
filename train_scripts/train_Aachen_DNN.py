12682d22163f2c56b976fb61222c6e7f69835b56
Jan <uqdqa@student.kit.edu>
2018-11-14 08:37:16 +0100

    fixed (real version)

--- train_scripts/train_Aachen_DNN.py
+++ train_scripts/train_Aachen_DNN.py
@@ -12,37 +12,45 @@ sys.path.append(basedir)
 import DRACO_Frameworks.DNN_Aachen.DNN_Aachen as DNN_Aachen
 import DRACO_Frameworks.DNN_Aachen.variable_info as variable_info
 
-categories_dic = {"(N_Jets == 6 and N_BTagsM >= 3)": variable_info.variables_6j_3b,
-    			  "(N_Jets == 5 and N_BTagsM >= 3)": variable_info.variables_5j_3b,
-    			  "(N_Jets == 4 and N_BTagsM >= 3)": variable_info.variables_4j_3b,} 
+categories_dic = {
+    "(N_Jets == 4 and N_BTagsM >= 3)": [variable_info.variables_4j_3b, "4j_ge3t"],
+    "(N_Jets == 5 and N_BTagsM >= 3)": [variable_info.variables_5j_3b, "5j_ge3t"],
+    "(N_Jets == 6 and N_BTagsM >= 3)": [variable_info.variables_6j_3b, "5j_ge3t"]}
 
-event_category = ["(N_Jets == 6 and N_BTagsM >= 3)",
-				  "(N_Jets == 5 and N_BTagsM >= 3)",
-				  "(N_Jets == 4 and N_BTagsM >= 3)"]
+prenet_targets = [
+    "GenAdd_BB_inacceptance",
+    "GenAdd_B_inacceptance",
+    "GenHiggs_BB_inacceptance",
+    "GenHiggs_B_inacceptance",
+    "GenTopHad_B_inacceptance",
+    "GenTopHad_QQ_inacceptance",
+    "GenTopHad_Q_inacceptance",
+    "GenTopLep_B_inacceptance"]
 
-
-inPath = "/storage/9/jschindler/AachenDNN_files"
-outpath = "/storage/9/jschindler/Aachen_DNN"
 event_classes = ["ttHbb", "ttbb", "ttb", "tt2b", "ttcc", "ttlf"]
-category = event_category[0]
+
+inPath = "/storage/c/vanderlinden/DRACO-MLfoy/workdir/aachen_data/"
+
+for key in categories_dic:
+    outpath = "/storage/c/vanderlinden/DRACO-MLfoy/workdir/aachen_eval_"+str(categories_dic[key][1])+"/"
 
 
+    dnn_aachen = DNN_Aachen.DNN(
+        in_path             = inPath, 
+        save_path           = outpath,		
+        event_classes       = event_classes, 
+        event_category      = key,
+        train_variables     = categories_dic[key][0], 
+        prenet_targets      = prenet_targets,
+        train_epochs        = 500,
+        early_stopping      = 50,
+        eval_metrics        = ["acc", "mean_squared_error"])
 
-dnn_aachen = DNN_Aachen.DNN(in_path=inPath, 
-							save_path=outpath,
-                			event_classes=event_classes, 
-                			event_category=category,
-                			train_variables=categories_dic[category], 
-                			prenet_targets=["GenAdd_BB_inacceptance",
-									        "GenAdd_B_inacceptance",
-									        "GenHiggs_BB_inacceptance",
-									        "GenHiggs_B_inacceptance",
-									        "GenTopHad_B_inacceptance",
-									        "GenTopHad_QQ_inacceptance",
-									        "GenTopHad_Q_inacceptance",
-									        "GenTopLep_B_inacceptance"]) # placeholders )
 
-dnn_aachen.build_model()
-dnn_aachen.train_models()
-
+    dnn_aachen.build_model()
+    dnn_aachen.train_models()
+    dnn_aachen.eval_model()
+    dnn_aachen.plot_metrics()
+    dnn_aachen.plot_prenet_nodes()
+    dnn_aachen.plot_classification_nodes()
 
