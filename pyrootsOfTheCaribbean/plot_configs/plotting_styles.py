4c6e22d889190f93f622560c84e64f0137de9463
Jan <uqdqa@student.kit.edu>
2018-11-14 19:29:40 +0100

    rootpy plotting (untested)

---
+++ pyrootsOfTheCaribbean/plot_configs/plotting_styles.py
@@ -0,0 +1,86 @@
+import rootpy.plotting as rp
+
+# dictionary for colors
+def get_plot_color( cls ):
+    color_dict = {
+        "ttH":   "royalblue",
+        "ttlf":  "salmon",
+        "ttcc":  "tomato",
+        "ttbb":  "brown",
+        "tt2b":  "darkred",
+        "ttb":   "red"
+        }
+    if "ttH" in cls: cls = "ttH"
+    return color_dict[cls]
+
+# intialize plotting style
+def init_plot_style():
+    rp.style.get_style("ATLAS")
+    style.SetEndErrorSize(3)
+    rp.style.set_style(style)
+
+# define style of signal histogram
+def set_sig_hist_style( hist, cls ):
+    hist.Sumw2()
+    hist.markersize = 0
+    hist.drawstyle = "shape"
+    hist.legendstyle = "L"
+    hist.fillstyle = "hollow"
+    hist.linestyle = "solid"
+    hist.linecolor = get_plot_color(cls)
+    hist.linewidth = 1.5
+
+# define style of background histogram
+def set_bkg_hist_style( hist, cls ):
+    hist.Sumw2()
+    hist.markersize = 0
+    hist.legendstyle = "F"
+    hist.fillstyle = "solid"
+    hist.fillcolor = get_plot_color(cls)
+    hist.linecolor = "black"
+    hist.linewidth = 1
+    
+# define style of data points
+def set_data_hist_style( hist, cls ):
+    print("TODO datastyle")
+    return
+
+
+# create canvas
+def init_canvas( ratiopad = False, logY = False ):
+    if not ratiopad:
+        canvas = rp.Canvas(width = 1024, height = 768)
+        canvas.SetTopMargin(0.07)
+        canvas.SetBottomMargin(0.15)
+        canvas.SetRightMargin(0.05)
+        canvas.SetLeftMargin(0.15)
+        canvas.SetTicks(1,1)
+    else:
+        print("TODO ratiopad")
+        canvas = None
+
+    if logY: canvas.cd(1).SetLogy()
+    
+    return canvas
+
+# create legend
+def init_legend(hists):
+    legend = rp.Legend(hists, entryheight = 0.03)
+    legend.SetX1NDC(0.90)
+    legend.SetX2NDC(0.99)
+    legend.SetY1NDC(0.90)
+    legend.SetY2NDC(0.99)
+    legend.SetBorderSize(0)
+    legend.SetLineStyle(0)
+    legend.SetTextSize(0.04)
+    legend.SetTextFont(42)
+    legend.SetFillStyle(0)
+    legend.Draw()
+    
+    return legend
+
+def save_canvas(canvas, save_path, clear_canvas = True):
+    canvas.Modified()
+    canvas.Update()
+    canvas.SaveAs(save_path)
+    if clear_canvas: canvas.Clear()
