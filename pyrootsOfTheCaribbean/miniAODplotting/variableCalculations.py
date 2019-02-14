f6eedef3937205dfb2c09faa54e0eba5bbc42f8c
Jan van der Linden <jan.linden@student.kit.edu>
2019-02-14 14:51:35 +0100

    calculation of helicity variables and other stuff

--- pyrootsOfTheCaribbean/miniAODplotting/variableCalculations.py
+++ pyrootsOfTheCaribbean/miniAODplotting/variableCalculations.py
@@ -43,7 +43,7 @@ def calculateVariables(evt):
         #event_data["theta_{}".format(obj1)] =       [evt.get_theta(obj1)]
         #event_data["costheta_{}".format(obj1)] =    [evt.get_costheta(obj1)]
         # polar angle
-        event_data["phi_{}".format(obj1)] =         [evt.get_phi(obj1)]
+        #event_data["phi_{}".format(obj1)] =         [evt.get_phi(obj1)]
         # mass of particle
         #event_data["mass_{}".format(obj1)] =        [evt.get_mass(obj1)]
 
@@ -58,9 +58,9 @@ def calculateVariables(evt):
             #event_data["dTheta_{}_{}".format(obj1,obj2)] =      [evt.get_dTheta(obj1,obj2)]
             #event_data["dcosTheta_{}_{}".format(obj1,obj2)] =   [evt.get_dcosTheta(obj1,obj2)]
             # difference in polar angle
-            event_data["dPhi_{}_{}".format(obj1,obj2)] =        [evt.get_dPhi(obj1,obj2)]
+            #event_data["dPhi_{}_{}".format(obj1,obj2)] =        [evt.get_dPhi(obj1,obj2)]
             # spacial difference in R = sqrt(dEta^2+dPhi^2)
-            event_data["dR_{}_{}".format(obj1,obj2)] =          [evt.get_dR(obj1,obj2)]
+            #event_data["dR_{}_{}".format(obj1,obj2)] =          [evt.get_dR(obj1,obj2)]
 
     
     # get the invariant ttbar system by adding both the top quarks
@@ -72,7 +72,7 @@ def calculateVariables(evt):
     #event_data["y_{}".format("ttbar")] =        [ttbar.Rapidity()]
     #event_data["theta_{}".format("ttbar")] =    [ttbar.Theta()]
     #event_data["costheta_{}".format("ttbar")] = [np.cos(ttbar.Theta())]
-    event_data["phi_{}".format("ttbar")] =      [ttbar.Phi()]
+    #event_data["phi_{}".format("ttbar")] =      [ttbar.Phi()]
     #event_data["mass_{}".format("ttbar")] =     [ttbar.M()]
 
     '''
@@ -135,6 +135,12 @@ def calculate_full_angles(event_data, evt):
     event_data["dXi_lepTop_lepB"] = leptop.Angle(lepb.Vect())
     event_data["dXi_Lepton_lepTop"] = lepton.Angle(leptop.Vect())
 
+    event_data["dcosXi_Lepton_hadB"] = np.cos(lepton.Angle(hadb.Vect()))
+    event_data["dcosXi_hadTop_lepTop"] = np.cos(hadtop.Angle(leptop.Vect()))
+    event_data["dcosXi_hadB_lepB"] = np.cos(hadb.Angle(lepb.Vect()))
+    event_data["dcosXi_hadTop_hadB"] = np.cos(hadtop.Angle(hadb.Vect()))
+    event_data["dcosXi_lepTop_lepB"] = np.cos(leptop.Angle(lepb.Vect()))
+    event_data["dcosXi_Lepton_lepTop"] = np.cos(lepton.Angle(leptop.Vect()))
     
 
     
@@ -154,6 +160,10 @@ def calculate_helicity_variables(event_data, evt):
 
     lepton = get_TLorentzVector(evt.objects["Lepton"])
 
+    ttbar_lepton = get_TLorentzVector(evt.objects["Lepton"])
+    ttbar_lepb = get_TLorentzVector(evt.objects["lepB"])
+    ttbar_hadb = get_TLorentzVector(evt.objects["hadB"])
+
     # ttbar system
     ttbar = hadtop+leptop
     boostvector_ttbar = ttbar.BoostVector()
@@ -167,6 +177,11 @@ def calculate_helicity_variables(event_data, evt):
     xyz_hadtop = hadtop.Vect()
     leptop.Boost(-boostvector_ttbar)
     xyz_leptop = leptop.Vect()
+    # boost lepton and bs in ttbar com frame
+    ttbar_lepton.Boost(-boostvector_ttbar)
+    ttbar_lepb.Boost(-boostvector_ttbar)
+    ttbar_hadb.Boost(-boostvector_ttbar)
+
     # boost ttbar into ttbar com frame
     ttbar.Boost(-boostvector_ttbar)
 
@@ -184,10 +199,16 @@ def calculate_helicity_variables(event_data, evt):
     event_data["com_ttbar_pT_hadTop"] = hadtop.Pt()
     event_data["com_ttbar_pT_lepTop"] = leptop.Pt()
 
+    event_data["com_ttbar_dXi_hadTop_hadB"] = hadtop.Angle(ttbar_hadb.Vect())
+    event_data["com_ttbar_dXi_lepTop_lepB"] = leptop.Angle(ttbar_lepb.Vect())
+    event_data["com_ttbar_dXi_lepTop_Lepton"] = leptop.Angle(ttbar_lepton.Vect())
+
+    event_data["com_ttbar_dcosXi_hadTop_hadB"] = np.cos(hadtop.Angle(ttbar_hadb.Vect()))
+    event_data["com_ttbar_dcosXi_lepTop_lepB"] = np.cos(leptop.Angle(ttbar_lepb.Vect()))
+    event_data["com_ttbar_dcosXi_lepTop_Lepton"] = np.cos(leptop.Angle(ttbar_lepton.Vect()))
+
     event_data["com_hadTop_pT_hadB"] = hadb.Pt()
     event_data["com_hadTop_P_hadB"] = hadb.P()
-    event_data["com_hadTop_costheta_hadB"] = np.cos(hadb.Theta())
-    event_data["com_hadTop_theta_hadB"] = hadb.Theta()
 
     event_data["com_hadTop_pT_hadW"] = hadw.Pt()
     event_data["com_hadTop_P_hadW"] = hadw.P()
@@ -204,6 +225,19 @@ def calculate_helicity_variables(event_data, evt):
     event_data["HA_lepB"] = ha_lepb
 
     event_data["HA_hadB_lepB"] = ha_lepb*ha_hadb
+
+    #event_data["com_ttbar_dXi_hadTop_lepTop"] = hadtop.Angle(leptop.Vect())
+    event_data["com_ttbar_dEta_hadTop_lepTop"] = abs(hadtop.Eta() - leptop.Eta())
+
+    dxi = hadb.Angle(lepb.Vect())
+    event_data["HF_dXi_hadB_lepB"] = dxi
+    event_data["HF_dcosXi_hadB_lepB"] = np.cos(dxi)
+
+    event_data["HF_dEta_hadB_lepB"] = abs(hadb.Eta() - lepb.Eta())
+    dphi = abs(hadb.Phi() - lepb.Phi())
+    if dphi > np.pi: dphi = 2.*np.pi-dphi
+    event_data["HF_dPhi_hadB_lepB"] = dphi
+
     return event_data
 
 def get_TLorentzVector(object):
