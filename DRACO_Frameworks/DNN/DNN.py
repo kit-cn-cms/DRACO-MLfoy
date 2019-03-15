a739d74328ce083044ac673c853e1a8fabef3880
Lea Reuter <lea.reuter@student.kit.edu>
2019-03-15 16:28:47 +0100

    deleted architecture.py structure

--- DRACO_Frameworks/DNN/DNN.py
+++ DRACO_Frameworks/DNN/DNN.py
@@ -72,12 +72,9 @@ class DNN():
             batch_size      = 5000,
             train_epochs    = 500,
             early_stopping  = 10,
-            optimizer       = None,
             loss_function   = "categorical_crossentropy",
             test_percentage = 0.2,
-            eval_metrics    = None,
-            additional_cut  = None,
-            use_pca         = False):
+            eval_metrics    = None):
 
         # save some information
         # list of samples to load into dataframe
@@ -110,15 +107,11 @@ class DNN():
         # additional metrics for evaluation of the training process
         self.eval_metrics = eval_metrics
 
-        # additional cuts to be applied after variable norm
-        self.additional_cut = additional_cut
-
-        # option for principle component analysis
-        self.PCA = use_pca
-
         # load data set
         self.data = self._load_datasets()
         self.event_classes = self.data.output_classes
+
+
 
         # save variable norm
         self.cp_path = self.save_path+"/checkpoints/"
@@ -133,16 +126,8 @@ class DNN():
         if not os.path.exists(self.plot_path):
             os.makedirs(self.plot_path)
 
-        # dict with architectures for analysis
-        self.architecture = arch.getArchitecture(self.JTstring)
         self.inputName = "inputLayer"
         self.outputName = "outputLayer"
-
-        # optimizer for training
-        if not(optimizer):
-            self.optimizer = self.architecture["optimizer"]
-        else:
-            self.optimizer = optimizer
 
         
     def _load_datasets(self):
@@ -153,9 +138,7 @@ class DNN():
             event_category      = self.event_category,
             train_variables     = self.train_variables,
             test_percentage     = self.test_percentage,
-            norm_variables      = True,
-            use_pca             = self.PCA,
-            additional_cut      = self.additional_cut)
+            norm_variables      = True)
 
 
         
@@ -266,7 +249,7 @@ class DNN():
         # compile the model
         model.compile(
             loss = self.architecture["loss_function"],
-            optimizer = self.optimizer,
+            optimizer = self.architecture["optimizer"],
             metrics = self.eval_metrics)
 
         # save the model
@@ -383,27 +366,8 @@ class DNN():
             for im, metric in enumerate(self.eval_metrics):
                 print("model test {}: {}".format(metric, self.model_eval[im+1]))
 
-        # evaluate non trainiable data
-        print("loading non trainable data")
-        self.data.get_non_train_samples()
-        if len(self.data.non_train_samples) == 0:
-            print("... no additional data found")
-
-        for sample in self.data.non_train_samples:
-            sample.addPrediction( self.model, self.train_variables )
                 
         
-
-    def save_confusionMatrix(self, location, save_roc):
-        ''' save confusion matrix as a line in output file '''
-        flattened_matrix = self.confusion_matrix.flatten()
-        labels = ["{}_in_{}_node".format(pred, true) for true in self.event_classes for pred in self.event_classes]
-        data = {label: [float(flattened_matrix[i])] for i, label in enumerate(labels)}
-        data["ROC"] = [float(self.roc_auc_score)]
-        df = pd.DataFrame.from_dict(data)
-        with pd.HDFStore(location, "a") as store:
-            store.append("data", df, index = False)
-        print("saved confusion matrix at "+str(location))
 
     # --------------------------------------------------------------------
     # result plotting functions
@@ -464,7 +428,7 @@ class DNN():
 
 
 
-    def plot_outputNodes(self, log = False, cut_on_variable = None, plot_nonTrainData = False):
+    def plot_outputNodes(self, log = False, plot_nonTrainData = False):
         ''' plot distribution in outputNodes '''
         nbins = 20
         bin_range = [0., 1.]
@@ -481,10 +445,6 @@ class DNN():
             logscale            = log,
             plot_nonTrainData   = plot_nonTrainData)
 
-        if cut_on_variable:
-            plotNodes.set_cutVariable(
-                cutClass = cut_on_variable["class"],
-                cutValue = cut_on_variable["value"])
 
         plotNodes.set_printROCScore(True)
         plotNodes.plot(ratio = False)
@@ -510,148 +470,6 @@ class DNN():
         plotDiscrs.plot(ratio = False)
 
 
-    def plot_input_output_correlation(self, plot = False):
-
-        # get input variables from test set TODO get them unnormed
-        input_data = self.data.get_test_data(as_matrix = False, normed = False)
-
-        # initialize empty dataframe
-        df = pd.DataFrame()
-        plt.figure(figsize = [10,10])
-
-        # correlation plot path
-        plt_path = self.save_path + "/correlations"
-        if not os.path.exists(plt_path):
-            os.makedirs(plt_path)
-
-        # loop over classes
-        for i_cls, cls in enumerate(self.event_classes):
-
-            # get predictions for current output note
-            pred_values = self.model_prediction_vector[:,i_cls]
-
-            # correlation values for class
-            corr_values = {}
-
-            # loop over input variables
-            for i_var, var in enumerate(self.train_variables):
-                # scatter plot:
-                # x-axis: input variable value
-                # y-axis: predicted discriminator output
-
-                var_values = input_data[var].values
-
-                assert( len(var_values) == len(pred_values) )
-
-                plt.hist2d(var_values, pred_values,
-                    bins = [min(binning.binning[var]["nbins"],20), 20],
-                    norm = LogNorm())
-                plt.colorbar()
-
-                # calculate correlation value
-                correlation = np.corrcoef(var_values, pred_values)[0][1]
-                print("correlation between {} and {}: {}".format(
-                    cls, var, correlation))
-
-                # write correlation value on plot
-                plt.title( correlation, loc = "left")
-                plt.xlabel(var)
-                plt.ylabel(cls+"_predicted")
-
-                out_name = plt_path + "/correlation_{}_{}.pdf".format(cls,var)
-                plt.savefig(out_name.replace("[","_").replace("]",""))
-                plt.clf()
-
-                corr_values[var] = correlation
-
-            # save correlation value to dataframe
-            df[cls] = pd.Series( corr_values )
-
-        # save dataframe of correlations
-        out_path = self.save_path + "/correlation_matrix.h5"
-        df.to_hdf(out_path, "correlations")
-        print("saved correlation matrix at "+str(out_path))
-
-
-    def plot_output_output_correlation(self, plot = False):
-        corr_path = self.save_path + "/output_correlations/"
-        if not os.path.exists(corr_path):
-            os.makedirs(corr_path)
-
-        correlation_matrix = []
-        for i_cls, xcls in enumerate(self.event_classes):
-            correlations = []
-            xvalues = self.model_prediction_vector[:,i_cls]
-
-            for j_cls, ycls in enumerate(self.event_classes):
-                yvalues = self.model_prediction_vector[:,j_cls]
-
-                corr = np.corrcoef( xvalues, yvalues)[0][1]
-                print("correlation between {} and {}: {}".format(xcls, ycls, corr))
-
-                correlations.append(corr)
-
-                if plot and i_cls < j_cls:
-                    plt.clf()
-                    plt.hist2d( xvalues, yvalues, bins = [20, 20],
-                        weights = self.data.get_lumi_weights(),
-                        norm = LogNorm(),
-                        cmap = "RdBu")
-                    plt.colorbar()
-
-                    plt.title("corr = {}".format(corr), loc = "left")
-                    plt.title(self.categoryLabel, loc = "right")
-
-                    plt.xlabel(xcls+" output node")
-                    plt.ylabel(ycls+" output node")
-
-                    out_name = corr_path + "/correlation_{}_{}.pdf".format(xcls, ycls)
-                    plt.savefig(out_name)
-
-            correlation_matrix.append(correlations)
-
-        # plot correlation matrix
-        n_classes = len(self.event_classes)
-
-        x = np.arange(0, n_classes+1, 1)
-        y = np.arange(0, n_classes+1, 1)
-
-        xn, yn = np.meshgrid(x,y)
-
-        plt.clf()
-        plt.figure(figsize = [10,10])
-        plt.pcolormesh(xn, yn, correlation_matrix, vmin = -1, vmax = 1)
-        plt.colorbar()
-
-        plt.xlim(0, n_classes)
-        plt.ylim(0, n_classes)
-
-        plt.xlabel("output nodes")
-        plt.ylabel("output nodes")
-
-        plt.title(self.categoryLabel, loc = "right")
-
-        # add textlabel
-        for yit in range(n_classes):
-            for xit in range(n_classes):
-                plt.text(xit+0.5,yit+0.5,
-                    "{:.3f}".format(correlation_matrix[yit][xit]),
-                    horizontalalignment = "center",
-                    verticalalignment = "center")
-
-        ax = plt.gca()
-        ax.set_xticks( np.arange((x.shape[0]-1))+0.5, minor = False)
-        ax.set_yticks( np.arange((y.shape[0]-1))+0.5, minor = False)
-
-        ax.set_xticklabels(self.event_classes)
-        ax.set_yticklabels(self.event_classes)
-
-        ax.set_aspect("equal")
-
-        out_path = self.save_path + "/output_correlation.pdf"
-        plt.savefig(out_path)
-        print("saved output correlation at "+str(out_path))
-        plt.clf()
 
     def plot_confusionMatrix(self, norm_matrix = True, privateWork = False, printROC = False):
         ''' plot confusion matrix '''
