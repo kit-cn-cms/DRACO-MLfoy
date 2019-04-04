939c85917bc6a2b70346d9a7800bc9f46f79c5b1
Angelagiraldi <angirald@naf-cms15.desy.de>
2019-04-04 12:31:27 +0200

    Added variables for DL categories

---
+++ variable_sets/DL_variables.py
@@ -0,0 +1,79 @@
+variables = {}
+variables["ge4j_ge4t"] = [
+    "MEM",
+    "HT_tags",
+    "btagDiscriminatorAverage_tagged",
+    "mass_tag_tag_min_deltaR",
+    "mass_jet_tag_min_deltaR",
+    "mass_jet_jet_jet_max_pT",
+    "pT_tag_tag_min_deltaR",
+    "avgDeltaR_tag_tag",
+    "minDeltaR_tag_tag",
+    "maxDeltaEta_jet_jet",
+    "maxDeltaEta_tag_tag",
+    "multiplicity_higgsLikeDijet15"
+    ]
+
+variables["ge4j_3t"] = [
+    "MEM",
+    "ptSum_jets_leptons",
+    "btagDiscriminatorAverage_untagged",
+    "btagDiscriminatorAverage_tagged",
+    "mass_tag_tag_min_deltaR",
+    "mass_tag_tag_max_mass",
+    "pT_tag_tag_min_deltaR",
+    "avgDeltaR_tag_tag",
+    "minDeltaR_tag_tag",
+    "maxDeltaEta_tag_tag",
+    "H0_jet",
+    "twist_tag_tag_max_mass"
+    ]
+
+variables["ge4j_2t"] = [
+    "btagDiscriminatorAverage_untagged",
+    "mass_tag_tag_min_deltaR",
+    "mass_jet_tag_min_deltaR",
+    "pT_tag_tag_min_deltaR",
+    "pT_jet_jet_min_deltaR",
+    "avgDeltaR_jet_jet",
+    "avgDeltaR_jet_tag",
+    "maxDeltaEta_jet_jet",
+    "C_jet",
+    "C_tag",
+    "R1_tag",
+    "multiplicity_higgsLikeDijet15"
+    ]
+
+variables["3j_3t"] = [
+    "ptSum_jets_leptons",
+    "btagDiscriminatorAverage_tagged",
+    "mass_higgsLikeDijet",
+    "mass_jet_jet_min_deltaR",
+    "mass_tag_tag_max_mass",
+    "pT_jet_jet_min_deltaR",
+    "avgDeltaR_jet_jet",
+    "minDeltaR_jet_jet",
+    "maxDeltaEta_jet_jet",
+    "C_jet",
+    "R1_jet",
+    "twist_jet_jet_max_mass"
+    ]
+
+variables["3j_2t"] = [
+    "HT_tags",
+    "ptSum_jets_leptons",
+    "btagDiscriminatorAverage_untagged",
+    "mass_jet_jet_min_deltaR",
+    "mass_tag_tag_min_deltaR",
+    "mass_jet_jet_jet_max_pT",
+    "pT_tag_tag_min_deltaR",
+    "avgDeltaR_jet_jet",
+    "minDeltaR_jet_jet",
+    "maxDeltaEta_jet_jet",
+    "C_jet",
+    "C_tag",
+    "H0_jet",
+    ]
+
+
+all_variables = set( [v for key in variables for v in variables[key] ] )
