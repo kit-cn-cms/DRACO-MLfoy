cb66717880ff2d8a3da8e8f57f7aaf0451c07cec
Jan van der Linden <jan.linden@student.kit.edu>
2019-04-12 18:46:14 +0200

    preprocessing for merged ttb categories for ttH2017

---
+++ preprocessing/root2pandas/preprocessing_merged.py
@@ -0,0 +1,152 @@
+import os
+import sys
+import optparse
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(os.path.dirname(filedir))
+sys.path.append(basedir)
+
+import root2pandas
+
+"""
+USE: python preprocessing.py --outputdirectory=DIR --variableSelection=FILE --maxentries=INT --MEM=BOOL
+"""
+usage="usage=%prog [options] \n"
+usage+="USE: python preprocessing.py --outputdirectory=DIR --variableselection=FILE --maxentries=INT --MEM=BOOL --name=STR\n"
+usage+="OR: python preprocessing.py -o DIR -v FILE -e INT -m BOOL -n STR"
+
+parser = optparse.OptionParser(usage=usage)
+
+parser.add_option("-o", "--outputdirectory", dest="outputDir",default="InputFeatures",
+        help="DIR for output", metavar="outputDir")
+
+parser.add_option("-v", "--variableselection", dest="variableSelection",default="example_variables",
+        help="FILE for variables used to train DNNs", metavar="variableSelection")
+
+parser.add_option("-e", "--maxentries", dest="maxEntries", default=50000,
+        help="INT used for maximal number of entries for each batch (to restrict memory usage)", metavar="maxEntries")
+
+parser.add_option("-m", "--MEM", dest="MEM", action = "store_true", default=False,
+        help="BOOL to use MEM or not", metavar="MEM")
+
+parser.add_option("-n", "--name", dest="Name", default="dnn",
+        help="STR of the output file name", metavar="Name")
+
+
+(options, args) = parser.parse_args()
+
+if not os.path.isabs(options.variableSelection):
+    sys.path.append(basedir+"/variable_sets/")
+    variable_set = __import__(options.variableSelection)
+elif os.path.exists(options.variableSelection):
+    variable_set = __import__(options.variableSelection)
+else:
+    sys.exit("ERROR: Variable Selection File does not exist!")
+
+if not os.path.isabs(options.outputDir):
+    outputdir = basedir+"/workdir/"+options.outputDir
+elif os.path.exists(options.outputDir) or os.path.exists(os.path.dirname(options.outputDir)):
+    outputdir=options.outputDir
+else:
+    sys.exit("ERROR: Output Directory does not exist!")
+
+# define a base event selection which is applied for all Samples
+# select only events with GEN weight > 0 because training with negative weights is weird
+base = "(N_Jets >= 4 and N_BTagsM >= 3 and Evt_Pt_MET > 20. and Weight_GEN_nom > 0.)"
+
+# single lepton selections
+single_mu_sel = "(N_LooseElectrons == 0 and N_TightMuons == 1 and Muon_Pt > 29. and Triggered_HLT_IsoMu27_vX == 1)"
+single_el_sel = "(N_LooseMuons == 0 and N_TightElectrons == 1 and (Triggered_HLT_Ele35_WPTight_Gsf_vX == 1 or Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX == 1))"
+
+base_selection = "("+base+" and ("+single_mu_sel+" or "+single_el_sel+"))"
+
+# define output classes
+ttH_categories = root2pandas.EventCategories()
+ttH_categories.addCategory("ttHbb", selection = None)
+
+
+ttbar_categories = root2pandas.EventCategories()
+ttbar_categories.addCategory("ttb", selection = "(GenEvt_I_TTPlusBB >= 1 and GenEvt_I_TTPlusCC == 0)")
+#ttbar_categories.addCategory("ttbb", selection = "(GenEvt_I_TTPlusBB == 3 and GenEvt_I_TTPlusCC == 0)")
+#ttbar_categories.addCategory("tt2b", selection = "(GenEvt_I_TTPlusBB == 2 and GenEvt_I_TTPlusCC == 0)")
+#ttbar_categories.addCategory("ttb",  selection = "(GenEvt_I_TTPlusBB == 1 and GenEvt_I_TTPlusCC == 0)")
+ttbar_categories.addCategory("ttlf", selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 0)")
+ttbar_categories.addCategory("ttcc", selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 1)")
+
+
+# initialize dataset class
+dataset = root2pandas.Dataset(
+    outputdir   = outputdir,
+    naming      = options.Name,
+    addMEM      = options.MEM,
+    maxEntries  = options.maxEntries)
+
+# add base event selection
+dataset.addBaseSelection(base_selection)
+
+
+
+ntuplesPath = "/nfs/dust/cms/user/vdlinden/ttH_2018/ntuples/ntuples_v5_forDNN/"
+memPath = "/nfs/dust/cms/user/mwassmer/ttH_2018/MEMs_v2/"
+
+# add samples to dataset
+dataset.addSample(
+    sampleName  = "ttHbb",
+    ntuples     = ntuplesPath+"/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8_new_pmx/*nominal*.root",
+    categories  = ttH_categories,
+    selections  = None,
+    MEMs        = memPath+"/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8/*.root",
+    even_odd    = True
+    ) 
+dataset.addSample(
+    sampleName  = "ttHNonbb",
+    ntuples     = ntuplesPath+"/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8_new_pmx/*nominal*.root",
+    categories  = ttH_categories,
+    selections  = None,
+    MEMs        = memPath+"/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/*.root",
+    even_odd    = True
+    ) 
+
+dataset.addSample(
+    sampleName  = "TTToSL",
+    ntuples     = ntuplesPath+"/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_new_pmx/*nominal*.root",
+    categories  = ttbar_categories,
+    selections  = None,#ttbar_selection,
+    MEMs        = memPath+"/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*.root",
+    )
+dataset.addSample(
+    sampleName  = "TTToDL",
+    ntuples     = ntuplesPath+"/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_new_pmx/*nominal*.root",
+    categories  = ttbar_categories,
+    selections  = None,#ttbar_selection,
+    MEMs        = memPath+"/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/*.root",
+    )
+dataset.addSample(
+    sampleName  = "TTToFH",
+    ntuples     = ntuplesPath+"/TTToHadronic_TuneCP5_13TeV-powheg-pythia8_new_pmx/*nominal*.root",
+    categories  = ttbar_categories,
+    selections  = None,#ttbar_selection,
+    MEMs        = memPath+"/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/*.root",
+    )
+
+# initialize variable list 
+dataset.addVariables(variable_set.all_variables)
+
+# define an additional variable list
+additional_variables = [
+    "N_Jets",
+    "N_BTagsM",
+    "N_BTagsT",
+    "N_BTagsL",
+    "Weight_XS",
+    "Weight_CSV",
+    "Weight_GEN_nom",
+    "Evt_ID", 
+    "Evt_Run", 
+    "Evt_Lumi"]
+
+# add these variables to the variable list
+dataset.addVariables(additional_variables)
+
+# run the preprocessing
+dataset.runPreprocessing()
