dbca19d0a675e6051525e830cf08846aec3faa2f
Jan <uqdqa@student.kit.edu>
2019-04-30 13:48:31 +0200

    variableset of 2017 analysis adjusted to legacy names

--- variable_sets/ttH_2018_variables.py
+++ variable_sets/ttH_2018_variables.py
@@ -1,39 +1,39 @@
 variables = {}
 variables["4j_ge3t"] = [
-    "Jet_CSV[3]",
+    "Jet_DeepJetCSV[3]",
     "CSV[1]",
     "N_BTagsT",
-    "BDT_common5_input_dev_from_avg_disc_btags",
-    "BDT_common5_input_sphericity_jets",
-    "Evt_blr_ETH",
-    "memDBp",
-    "BDT_common5_input_transverse_sphericity_jets",
+    "MVA_dev_from_avg_disc_btags",
+    "MVA_sphericity_jets",
+    "MVA_blr",
+    #"memDBp",
+    "MVA_transverse_sphericity_jets",
     "Evt_CSV_Min_Tagged",
     "Evt_CSV_Average_Tagged",
-    "Evt_CSV_Average",
-    "Evt_blr_ETH_transformed",
-    "BDT_common5_input_HT_tag",
-    "Evt_M2_TaggedJetsAverage",
+    "MVA_Evt_CSV_Average",
+    "MVA_blr_transformed",
+    "MVA_HT_tag",
+    "MVA_Evt_M2_TaggedJetsAverage",
     ]
 
 
 
 variables["5j_ge3t"] = [
-    "BDT_common5_input_max_dR_jj",
+    "MVA_max_dR_jj",
     "CSV[1]",
     "N_BTagsT",
     "Evt_Dr_MinDeltaRLeptonTaggedJet",
-    "BDT_common5_input_sphericity_jets",
+    "MVA_sphericity_jets",
     "Jet_Pt[2]",
-    "BDT_common5_input_closest_tagged_dijet_mass",
-    "BDT_common5_input_sphericity_tags",
+    "MVA_closest_tagged_dijet_mass",
+    "MVA_sphericity_tags",
     "Jet_Pt[0]",
-    "BDT_common5_input_HT_tag",
+    "MVA_HT_tag",
     "Evt_Dr_TaggedJetsAverage",
-    "memDBp",
+    #"memDBp",
     "Evt_CSV_Average_Tagged",
     "Evt_CSV_Min_Tagged",
-    "Evt_CSV_Average",
+    "MVA_Evt_CSV_Average",
     ]
 
 
@@ -43,14 +43,14 @@ variables["ge6j_ge3t"] = [
     "Evt_Dr_TaggedJetsAverage",
     "Evt_M_MinDeltaRLeptonTaggedJet",
     "CSV[1]",
-    "BDT_common5_input_dev_from_avg_disc_btags",
-    "Evt_blr_ETH",
-    "Evt_CSV_Average",
+    "MVA_dev_from_avg_disc_btags",
+    "MVA_blr",
+    "MVA_Evt_CSV_Average",
     "Evt_CSV_Average_Tagged",
-    "BDT_common5_input_HT_tag",
+    "MVA_HT_tag",
     "Evt_M_JetsAverage",
-    "memDBp",
-    "Evt_blr_ETH_transformed",
+    #"memDBp",
+    "MVA_blr_transformed",
     ]
 
 
