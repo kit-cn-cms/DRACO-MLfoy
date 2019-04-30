1c6f31d41b521ca1147ff497d8c9b1c0680bc793
Jan <uqdqa@student.kit.edu>
2019-04-30 13:48:02 +0200

    something

--- train_scripts/net_configs.py
+++ train_scripts/net_configs.py
@@ -27,7 +27,7 @@ config_dict["test_config"] = {
         "earlystopping_epochs":     50,
         }
 
-config_dict["ttH_2018"] = {
+config_dict["ttH_2017"] = {
         "layers":                   [100,100,100],
         "loss_function":            "categorical_crossentropy",
         "Dropout":                  0.50,
@@ -40,8 +40,21 @@ config_dict["ttH_2018"] = {
         "earlystopping_epochs":     100,
         }
 
-config_dict["ttZ_2018"] = {
+config_dict["legacy_2018"] = {
         "layers":                   [200,100,50],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.20,
+        "L2_Norm":                  1e-5,
+        "batch_size":               50000,
+        "optimizer":                optimizers.Adadelta(),
+        "activation_function":      "elu",
+        "output_activation":        "Softmax",
+        "earlystopping_percentage": 0.05,
+        "earlystopping_epochs":     100,
+        }
+
+config_dict["ttZ_2018"] = {
+        "layers":                   [100,100,100],
         "loss_function":            "categorical_crossentropy",
         "Dropout":                  0.5,
         "L2_Norm":                  1e-5,
