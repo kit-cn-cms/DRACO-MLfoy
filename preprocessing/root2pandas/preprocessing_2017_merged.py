1fa5a735f44192ec802fe6121a364f2643250542
sebwieland <wielandseb@googlemail.com>
2019-07-04 17:37:21 +0200

    stupid

--- preprocessing/root2pandas/preprocessing_2017_merged.py
+++ preprocessing/root2pandas/preprocessing_2017_merged.py
@@ -69,9 +69,6 @@ ttH_categories.addCategory("ttH", selection = None)
 
 
 ttlf_categories = root2pandas.EventCategories()
-ttbar_categories.addCategory("ttbb", selection = "(GenEvt_I_TTPlusBB == 3 and GenEvt_I_TTPlusCC == 0)")
-ttbar_categories.addCategory("tt2b", selection = "(GenEvt_I_TTPlusBB == 2 and GenEvt_I_TTPlusCC == 0)")
-ttbar_categories.addCategory("ttb",  selection = "(GenEvt_I_TTPlusBB == 1 and GenEvt_I_TTPlusCC == 0)")
 ttlf_categories.addCategory("ttlf", selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 0)")
 ttlf_categories.addCategory("ttcc", selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 1)")
 
