775eae60e757523eb8e0f3c9d3eb18a7d40539e2
Sebastian Wieland <swieland@ekplx92.ekp.kit.edu>
2019-07-10 14:45:42 +0200

    adjusted default net configs

--- train_scripts/net_configs.py
+++ train_scripts/net_configs.py
@@ -4,11 +4,23 @@ config_dict = {}
 config_dict["example_config_challenge"] = {
         "layers":                   [200,200],
         "loss_function":            "binary_crossentropy",
-        "Dropout":                  0.5,
+        "Dropout":                  0.3,
         "L2_Norm":                  1e-5,
-        "batch_size":               5000,
+        "batch_size":               10000,
         "optimizer":                optimizers.Adam(),
-        # "optimizer":                optimizers.Adagrad(decay=0.99),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.05,
+        "earlystopping_epochs":     50,
+        }
+
+config_dict["example_config_challenge_multiclass"] = {
+        "layers":                   [200,200],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.3,
+        "L2_Norm":                  1e-5,
+        "batch_size":               10000,
+        "optimizer":                optimizers.Adam(),
         "activation_function":      "elu",
         "output_activation":        "Sigmoid",
         "earlystopping_percentage": 0.05,
@@ -16,16 +28,14 @@ config_dict["example_config_challenge"] = {
         }
 
 config_dict["example_config_binary"] = {
-        "layers":                   [500,500],
+        "layers":                   [100,100],
         "loss_function":            "binary_crossentropy",
         "Dropout":                  0.3,
         "L2_Norm":                  1e-5,
         "batch_size":               500,
-        # "optimizer":                optimizers.Adagrad(decay=0.99),
         "optimizer":                optimizers.Adam(),
         "activation_function":      "elu",
         "output_activation":        "Sigmoid",
         "earlystopping_percentage": 0.05,
         "earlystopping_epochs":     50,
         }
-
