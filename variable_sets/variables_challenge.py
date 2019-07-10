ce3e1f482fba51b273d5a0bf92fd31fb970349fb
Sebastian Wieland <swieland@ekplx92.ekp.kit.edu>
2019-07-10 15:43:32 +0200

    reduce default input variables

--- variable_sets/variables_challenge.py
+++ variable_sets/variables_challenge.py
@@ -12,28 +12,33 @@ variables["ge6j_ge3t"] = [
     "Jet_CSV[1]",
     "Jet_CSV[2]",
 
-    "memDBp",
- 
-    "Evt_JetPtOverJetE",
-    "Evt_Dr_MinDeltaRTaggedJets",
-    "Evt_Dr_TaggedJetsAverage",
-
-    "BDT_common5_input_closest_tagged_dijet_mass",
-    "Evt_M_MinDeltaRLeptonTaggedJet",
-
-    "BDT_common5_input_sphericity_jets",
-    "BDT_common5_input_dev_from_avg_disc_btags",
-    "N_BTagsT",
-    "Evt_blr_ETH",
-    "Evt_Deta_TaggedJetsAverage",
-    "Evt_M2_TaggedJetsAverage",
-    "Evt_CSV_Average",
-    "Evt_CSV_Average_Tagged",
-    "Evt_CSV_Min_Tagged",
-    "BDT_common5_input_HT_tag",
-    "Evt_M_JetsAverage",
-    "Evt_blr_ETH_transformed",
     ]
 
 all_variables = list(set( [v for key in variables for v in variables[key] ] ))
 
+
+#######################
+#additional variables #
+#######################
+
+# "memDBp",
+
+# "Evt_JetPtOverJetE",
+# "Evt_Dr_MinDeltaRTaggedJets",
+# "Evt_Dr_TaggedJetsAverage",
+
+# "BDT_common5_input_closest_tagged_dijet_mass",
+# "Evt_M_MinDeltaRLeptonTaggedJet",
+
+# "BDT_common5_input_sphericity_jets",
+# "BDT_common5_input_dev_from_avg_disc_btags",
+# "N_BTagsT",
+# "Evt_blr_ETH",
+# "Evt_Deta_TaggedJetsAverage",
+# "Evt_M2_TaggedJetsAverage",
+# "Evt_CSV_Average",
+# "Evt_CSV_Average_Tagged",
+# "Evt_CSV_Min_Tagged",
+# "BDT_common5_input_HT_tag",
+# "Evt_M_JetsAverage",
+# "Evt_blr_ETH_transformed",
