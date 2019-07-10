6005864a72280d5882a3296aaf330595be199d3c
Sebastian Wieland <swieland@ekplx92.ekp.kit.edu>
2019-07-10 16:24:47 +0200

    added train script for ttbb

---
+++ train_scripts/train_challenge_ttbb.py
@@ -0,0 +1,114 @@
+# global imports
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+import os
+import sys
+
+# option handler
+import optionHandler
+options = optionHandler.optionHandler(sys.argv)
+
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(filedir)
+sys.path.append(basedir)
+
+# import class for DNN training
+import DRACO_Frameworks.DNN.DNN as DNN
+import DRACO_Frameworks.DNN.data_frame as df
+
+options.initArguments()
+dataPath = "/ceph/swieland/TP2-WZH/MLInput/train"
+
+# load samples
+input_samples = df.InputSamples(dataPath, options.getActivatedSamples(), options.getTestPercentage())
+
+# define all samples
+input_samples.addSample(options.getDefaultName("ttH"),  label = "ttH",  normalization_weight = options.getNomWeight())
+input_samples.addSample(options.getDefaultName("ttbb"), label = "ttbb", normalization_weight = options.getNomWeight())
+
+if options.isBinary():
+    input_samples.addBinaryLabel(options.getSignal(), options.getBinaryBkgTarget())
+
+# initializing DNN training class
+dnn = DNN.DNN(
+    save_path       = options.getOutputDir(),
+    input_samples   = input_samples,
+    event_category  = "ge6j_ge3t",
+    train_variables = options.getTrainVariables(),
+    # number of epochs
+    train_epochs    = options.getTrainEpochs(),
+    # metrics for evaluation (c.f. KERAS metrics)
+    eval_metrics    = ["acc"],
+    # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+    test_percentage = options.getTestPercentage(),
+    # balance samples per epoch such that there amount of samples per category is roughly equal
+    balanceSamples  = False,
+    evenSel         = options.doEvenSelection(),
+    norm_variables  = options.doNormVariables())
+
+# build DNN model
+dnn.build_model(options.getNetConfig())
+
+# perform the training
+dnn.train_model()
+
+# evalute the trained model
+dnn.eval_model()
+
+# save information
+dnn.save_model(sys.argv, filedir)
+
+# save and print variable ranking
+dnn.get_input_weights()
+
+# plotting 
+if options.doPlots():
+    # plot the evaluation metrics
+    dnn.plot_metrics(privateWork = options.isPrivateWork())
+
+    if options.isBinary():
+        # plot output node
+        bin_range = options.getBinaryBinRange()
+        dnn.plot_binaryOutput(
+            log         = options.doLogPlots(), 
+            privateWork = options.isPrivateWork(), 
+            printROC    = options.doPrintROC(), 
+            bin_range   = bin_range, 
+            name        = options.getName())
+    else:
+        # plot the confusion matrix
+        dnn.plot_confusionMatrix(
+            privateWork = options.isPrivateWork(), 
+            printROC    = options.doPrintROC())
+
+        # plot the output discriminators
+        dnn.plot_discriminators(
+            log                 = options.doLogPlots(), 
+            signal_class        = options.getSignal(),         
+            privateWork         = options.isPrivateWork(), 
+            printROC            = options.doPrintROC(),    
+            sigScale            = options.getSignalScale())
+
+        # plot the output nodes
+        dnn.plot_outputNodes(
+            log                 = options.doLogPlots(), 
+            signal_class        = options.getSignal(), 
+            privateWork         = options.isPrivateWork(), 
+            printROC            = options.doPrintROC(), 
+            sigScale            = options.getSignalScale())
+
+        # plot event yields
+        dnn.plot_eventYields(
+            log                 = options.doLogPlots(), 
+            signal_class        = options.getSignal(), 
+            privateWork         = options.isPrivateWork(), 
+            sigScale            = options.getSignalScale())
+
+        # plot closure test
+        dnn.plot_closureTest(
+            log                 = options.doLogPlots(), 
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork())
+
+
