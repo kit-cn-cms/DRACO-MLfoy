f69beb69f2b498082df2e0e264b7ecd5fdfdbf73
pekraem <upcnb@student.kit.edu>
2019-08-16 12:08:33 +0200

    script to add .h5 files

---
+++ utils/addHDF.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+import sys
+import optparse
+
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(filedir)
+sys.path.append(basedir)
+
+usage = ""
+parser = optparse.OptionParser(usage=usage)
+
+parser.add_option("-o", "--output", dest="outFile", default="mergedHDFfiles",
+    help="Path and name of the new file where the merged data shall be saved", metavar="outFile")
+
+parser.add_option("-i", "--input", dest="inFiles", default=None,
+    help="comma separated list with all .h5 files to add", metavar="inFiles")
+
+parser.add_option("-d", "--directory", dest="directory", default=None,
+    help="adds directory from path to systems working directory", metavar="directory")
+
+(options, args) = parser.parse_args()
+
+if not os.path.isabs(options.directory):
+    sys.path.append(basedir+options.directory+"/")
+else:
+    sys.path.append(directory+"/")
+
+inList=options.inFiles.split(",")
+
+if os.path.exists(options.outFile):
+    print("renaming file {}".format(options.outFile))
+    os.rename(options.outFile,options.outFile+".old")
+
+with pd.HDFStore(options.outFile, "a") as store:
+    for h5file in inList:
+        if options.directory:  
+            store.append("data", pd.read_hdf(os.path.join(options.directory+"/",h5file)))
+        else:
+            store.append("data", pd.read_hdf(h5file), index=False)
