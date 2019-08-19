f89fc0195a267cf101d88d3f7d1ffe4fb4fd3c38
pekraem <upcnb@student.kit.edu>
2019-08-19 13:54:35 +0200

    class_label and path for output

--- utils/addHDF.py
+++ utils/addHDF.py
@@ -11,34 +11,39 @@ sys.path.append(basedir)
 usage = ""
 parser = optparse.OptionParser(usage=usage)
 
-parser.add_option("-o", "--output", dest="outFile", default="mergedHDFfiles",
-    help="Path and name of the new file where the merged data shall be saved", metavar="outFile")
+parser.add_option("-o", "--output", dest="outName", default="mergedHDFfiles",
+    help="Path and name of the new file where the merged data shall be saved", metavar="outName")
 
 parser.add_option("-i", "--input", dest="inFiles", default=None,
     help="comma separated list with all .h5 files to add", metavar="inFiles")
 
-parser.add_option("-d", "--directory", dest="directory", default=".",
-    help="adds directory from path to systems working directory", metavar="directory")
+parser.add_option("-id", "--inputDirectory", dest="inDirectory", default=".",
+    help="path to the directory where the input files are", metavar="inDirectory")
+
+parser.add_option("-od", "--outputDirectory", dest="outDirectory", default=".",
+    help="path to the directory where the input files are", metavar="outDirectory")
 
 parser.add_option("-n", "--naming", dest="naming", default="_dnn.h5",
     help="adds naming to input, so only ttlf... must be given", metavar="naming")
 
 (options, args) = parser.parse_args()
 
-if not os.path.isabs(options.directory):
-    sys.path.append(basedir+options.directory+"/")
+if not os.path.isabs(options.inDirectory):
+    sys.path.append(basedir+options.inDirectory+"/")
 else:
-    sys.path.append(directory+"/")
+    sys.path.append(options.inDirectory+"/")
 
 inList=options.inFiles.split(",")
 
-if os.path.exists(options.outFile):
-    print("renaming file {}".format(options.outFile))
-    os.rename(options.outFile,options.outFile+".old")
+if os.path.exists(options.outName+".h5"):
+    print("renaming file {}".format(options.outName))
+    os.rename(options.outName,options.outName+".old")
 
-with pd.HDFStore(options.outFile, "a") as store:
+with pd.HDFStore(options.outDirectory+"/"+options.outName+".h5", "a") as store:
     for h5file in inList:
-        if options.directory:  
-            store.append("data", pd.read_hdf(os.path.join(options.directory+"/",h5file+options.naming)), index=False)
+        if options.inDirectory:
+            tmp_df=pd.read_hdf(os.path.join(options.inDirectory+"/",h5file+options.naming))
         else:
-            store.append("data", pd.read_hdf(h5file), index=False)
+            tmp_df=pd.read_hdf(h5file)
+        tmp_df["class_label"] = pd.Series([options.outName]*tmp_df.shape[0], index = tmp_df.index) 
+        store.append("data", tmp_df, index=False)
