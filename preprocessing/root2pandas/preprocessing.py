f0cadf4b37ac56f03234dada66950fb5541c900f
Jan <uqdqa@student.kit.edu>
2019-09-06 13:32:25 +0200

    preprocessing updated

--- preprocessing/root2pandas/preprocessing.py
+++ preprocessing/root2pandas/preprocessing.py
@@ -17,10 +17,10 @@ usage+="OR: python preprocessing.py -o DIR -v FILE -e INT -m BOOL -n STR"
 
 parser = optparse.OptionParser(usage=usage)
 
-parser.add_option("-o", "--outputdirectory", dest="outputDir",default="InputFeatures",
+parser.add_option("-o", "--outputdirectory", dest="outputDir",default="/nfs/dust/cms/user/vdlinden/ttZAnalysis/DNNInputs",
         help="DIR for output", metavar="outputDir")
 
-parser.add_option("-v", "--variableselection", dest="variableSelection",default="example_variables",
+parser.add_option("-v", "--variableselection", dest="variableSelection",default="allVariables",
         help="FILE for variables used to train DNNs", metavar="variableSelection")
 
 parser.add_option("-e", "--maxentries", dest="maxEntries", default=50000,
@@ -32,6 +32,8 @@ parser.add_option("-m", "--MEM", dest="MEM", action = "store_true", default=Fals
 parser.add_option("-n", "--name", dest="Name", default="dnn",
         help="STR of the output file name", metavar="Name")
 
+parser.add_option("--odd",dest="even_odd_splitting",default=False,action="store_true",
+        help="BOOL to activate even odd splitting (only process events with 'Evt_Odd==1'). default is FALSE (all events are processed)")
 
 (options, args) = parser.parse_args()
 
@@ -52,28 +54,25 @@ else:
 
 # define a base event selection which is applied for all Samples
 # select only events with GEN weight > 0 because training with negative weights is weird
-base = "(N_Jets >= 4 and N_BTagsM >= 3 and Evt_Pt_MET > 20. and Weight_GEN_nom > 0.)"
+base = "(N_Jets >= 4 and N_BTagsM >= 3 and Evt_MET_Pt > 20. and Weight_GEN_nom > 0.)"
 
 # single lepton selections
-single_mu_sel = "(N_LooseElectrons == 0 and N_TightMuons == 1 and Muon_Pt > 29. and Triggered_HLT_IsoMu27_vX == 1)"
-single_el_sel = "(N_LooseMuons == 0 and N_TightElectrons == 1 and (Triggered_HLT_Ele35_WPTight_Gsf_vX == 1 or Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX == 1))"
+single_mu_sel = "(N_LooseElectrons == 0 and N_TightMuons == 1 and Triggered_HLT_IsoMu24_vX == 1)"
+single_el_sel = "(N_LooseMuons == 0 and N_TightElectrons == 1 and (Triggered_HLT_Ele32_WPTight_Gsf_vX == 1 or Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150 == 1))"
 
 base_selection = "("+base+" and ("+single_mu_sel+" or "+single_el_sel+"))"
-
-ttH_selection = "(Evt_Odd == 1)"
 
 # define output classes
 ttH_categories = root2pandas.EventCategories()
 ttH_categories.addCategory("ttH", selection = None)
 
-
 ttbar_categories = root2pandas.EventCategories()
-ttbar_categories.addCategory("ttbb", selection = "(GenEvt_I_TTPlusBB == 3 and GenEvt_I_TTPlusCC == 0)")
-ttbar_categories.addCategory("tt2b", selection = "(GenEvt_I_TTPlusBB == 2 and GenEvt_I_TTPlusCC == 0)")
-ttbar_categories.addCategory("ttb",  selection = "(GenEvt_I_TTPlusBB == 1 and GenEvt_I_TTPlusCC == 0)")
-ttbar_categories.addCategory("ttlf", selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 0)")
-ttbar_categories.addCategory("ttcc", selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 1)")
+ttbar_categories.addCategory("ttbb",        selection = "(GenEvt_I_TTPlusBB >= 1 and GenEvt_I_TTPlusCC == 0)")
+ttbar_categories.addCategory("ttlf",        selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 0)")
+ttbar_categories.addCategory("ttcc",        selection = "(GenEvt_I_TTPlusBB == 0 and GenEvt_I_TTPlusCC == 1)")
 
+ttZ_categories = root2pandas.EventCategories()
+ttZ_categories.addCategory("ttZ", selection = None)
 
 # initialize dataset class
 dataset = root2pandas.Dataset(
@@ -87,32 +86,43 @@ dataset.addBaseSelection(base_selection)
 
 
 
-ntuplesPath = "/nfs/dust/cms/user/vdlinden/ttH_2018/ntuples/ntuples_v5_forDNN/"
-memPath = "/nfs/dust/cms/user/mwassmer/ttH_2018/MEMs_v2/"
+ntuplesPath = "/nfs/dust/cms/user/vdlinden/ttZAnalysis/ntuples/"
 
 # add samples to dataset
 dataset.addSample(
-    sampleName  = "ttHbb",
-    ntuples     = ntuplesPath+"/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8_new_pmx/*nominal*.root",
+    sampleName  = "ttH",
+    ntuples     = ntuplesPath+"/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8/*nominal*.root",
     categories  = ttH_categories,
-    selections  = ttH_selection,
-    MEMs        = memPath+"/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8/*.root",
-   ) 
+    even_odd    = options.even_odd_splitting,
+    selections  = "(Evt_Odd==1)",
+    ) 
 
 dataset.addSample(
-    sampleName  = "TTToSL",
-    ntuples     = ntuplesPath+"/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_new_pmx/*nominal*.root",
+    sampleName  = "ttbar",
+    ntuples     = ntuplesPath+"/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*nominal*.root",
     categories  = ttbar_categories,
-    selections  = None,#ttbar_selection,
-    MEMs        = memPath+"/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*.root",
-      )
+    even_odd    = options.even_odd_splitting,
+    selections  = "(Evt_Odd==1)"
+    )
+
+dataset.addSample(
+    sampleName  = "ttZ",
+    ntuples     = ntuplesPath+"/TTZToBB_TuneCP5_13TeV-amcatnlo-pythia8/*nominal*.root",
+    categories  = ttZ_categories,
+    even_odd    = options.even_odd_splitting,
+    selections  = "(GenEvt_I_TTZ==1)"
+    )
+
 # initialize variable list 
 dataset.addVariables(variable_set.all_variables)
 
 # define an additional variable list
 additional_variables = [
+    "Evt_Odd",
     "N_Jets",
+    "N_BTagsL",
     "N_BTagsM",
+    "N_BTagsT",
     "Weight_XS",
     "Weight_CSV",
     "Weight_GEN_nom",
