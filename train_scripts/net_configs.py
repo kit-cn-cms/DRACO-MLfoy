c7463d698b5db4600bf7761279e9946b509c35ee
Jan <uqdqa@student.kit.edu>
2019-09-17 17:48:58 +0200

    typo update

--- train_scripts/net_configs.py
+++ train_scripts/net_configs.py
@@ -5,47 +5,9 @@ config_dict = {}
 config_dict["ttZAnalysis"] = {
         "layers":                   [50,50],
         "loss_function":            "categorical_crossentropy",
-        "Dropout":                  0.4,
-        "L2_Norm":                  1e-5,
-        "batch_size":               200,
-        "optimizer":                optimizers.Adagrad(),
-        "activation_function":      "leakyrelu",
-        "output_activation":        "Softmax",
-        "earlystopping_percentage": 0.05,
-        "earlystopping_epochs":     10,
-        }
-
-config_dict["ttZAnalysis2"] = {
-        "layers":                   [50,50],
-        "loss_function":            "categorical_crossentropy",
         "Dropout":                  0.3,
         "L2_Norm":                  1e-5,
         "batch_size":               200,
-        "optimizer":                optimizers.Adagrad(),
-        "activation_function":      "leakyrelu",
-        "output_activation":        "Softmax",
-        "earlystopping_percentage": 0.05,
-        "earlystopping_epochs":     10,
-        }
-
-config_dict["ttZAnalysis3"] = {
-        "layers":                   [50,50],
-        "loss_function":            "categorical_crossentropy",
-        "Dropout":                  0.3,
-        "L2_Norm":                  1e-5,
-        "batch_size":               2000,
-        "optimizer":                optimizers.Adagrad(),
-        "activation_function":      "leakyrelu",
-        "output_activation":        "Softmax",
-        "earlystopping_percentage": 0.05,
-        "earlystopping_epochs":     10,
-        }
-config_dict["meme"] = {
-        "layers":                   [1000 for _ in range(10)],
-        "loss_function":            "categorical_crossentropy",
-        "Dropout":                  0.3,
-        "L2_Norm":                  1e-5,
-        "batch_size":               2000,
         "optimizer":                optimizers.Adagrad(),
         "activation_function":      "leakyrelu",
         "output_activation":        "Softmax",
