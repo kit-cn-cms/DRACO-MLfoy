09fd73e36301dc1a858103eaaa9f556fd3c1767b
Julia Vautz <jvautz@ekplx86.physik.uni-karlsruhe.de>
2020-04-06 18:36:41 +0200

    alternative second channel CSV introduced

---
+++ variable_sets/multichannel_cnn_CSV.py
@@ -0,0 +1,7 @@
+variables = {}
+variables["ge6j_ge3t"] = [
+    'Jet_Pt[0-16]_Hist',
+    'Jet_CSV[0-16]_Hist',
+    ]
+
+all_variables = list(set( [v for key in variables for v in variables[key] ] ))
---
+++ variable_sets/onechannel_cnn.py
@@ -0,0 +1,6 @@
+variables = {}
+variables["ge6j_ge3t"] = [
+    'Jet_Pt[0-16]_Hist',
+    ]
+
+all_variables = list(set( [v for key in variables for v in variables[key] ] ))
