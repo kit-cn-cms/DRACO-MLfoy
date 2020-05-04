f2e86a7b2d60f13a7039c3487c3bd47a97f7ce20
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-05-04 21:48:29 +0200

    Add possibility to plot variable ranking when only one BNN is available

--- utils/getTopVariables.py
+++ utils/getTopVariables.py
@@ -5,7 +5,13 @@ import pandas as pd
 import glob
 from collections import Counter
 import operator
-import matplotlib.pyplot as plt
+
+# so that matplotlib can be used over ssh
+import matplotlib #me
+matplotlib.use('Agg') #me
+import matplotlib.pyplot as plt #me
+
+
 import numpy as np
 import optparse
 
@@ -13,6 +19,9 @@ import optparse
 filedir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(filedir)
 sys.path.append(basedir)
+
+#cmd: python getTopVariables.py -w /home/ycung/Desktop/DRACO-MLfoy/workdir -i training_ANN/QT_ANN_training_*_JTSTRING -o /home/ycung/Desktop/DRACO-MLfoy/workdir/test_2 -p -t first_layer ge4j_ge3t
+#      python getTopVariables.py -w /home/ycung/Desktop/DRACO-MLfoy/workdir -i 16-04-2020/BNN_training_JTSTRING -o /home/ycung/Desktop/DRACO-MLfoy/workdir/variable_ranking_04-05-2020 -p -t first_layer ge4j_ge3t
 
 usage = "python getTopVariables.py [options] [jtCategories]"
 parser = optparse.OptionParser(usage=usage)
@@ -23,6 +32,8 @@ parser.add_option("-i","--input",dest="inputdir",default="test_training",
 for jet-tag categories and escaped wildcards ('\*') for multiple DNN runs.")
 parser.add_option("-o","--output",dest="outdir",default="./",
     help = "path to output directory e.g. for plots or variable sets")
+parser.add_option("--filename",dest="filename",default="",
+    help = "filename") #me
 parser.add_option("-p","--plot",dest="plot",default=False,action="store_true",
     help = "generate plots of variable rankings")
 parser.add_option("--nplot",dest="nplot",default=30,type=int,
@@ -55,6 +66,10 @@ for jtcat in args:
     
     if opts.taylor_expansion:
         jtpath+= "/checkpoints/keras_taylor_1D.csv"
+    
+    elif "BNN" in opts.inputdir:
+        jtpath+= "/"+opts.weight_type+"_weights.csv"
+
     else:
         # collect weight sum files
         jtpath+= "/"+opts.weight_type+"_weight_sums.csv"
@@ -106,36 +121,78 @@ for jtcat in args:
 
         sorted_variables[jtcat] = top_variables       
 
-    else:           
-        # collect variables and their relative importance
-        variables = {}
-        for ranking in rankings:
-            csv = pd.read_csv(ranking, header = 0, sep = ",", names = ["variable", "weight_sum"])
-            sum_of_weights = csv["weight_sum"].sum()
-            for row in csv.iterrows():
-                if not row[1][0] in variables: variables[row[1][0]] = []
-                variables[row[1][0]].append(row[1][1]/sum_of_weights)
+    else:
+        if "BNN" in opts.inputdir:
+            # collect variables and their relative importance
+            variables = {}
+            variables_std = {}
+
+            for ranking in rankings:
+                csv = pd.read_csv(ranking, header = 0, sep = ",", names = ["variable", "weight_mean_sum", "weight_std_sum"])
+                sum_of_weights = csv["weight_mean_sum"].sum()
+
+                for row in csv.iterrows():
+                    if not row[1][0] in variables: 
+                        variables[row[1][0]] = []
+                        variables_std[row[1][0]] = []
+
+                    variables[row[1][0]].append(row[1][1]/sum_of_weights)
+                    variables_std[row[1][0]].append(row[1][2]/sum_of_weights)
 
 
-        # collect mean values of variables
-        mean_dict = {}
-        for v in variables: mean_dict[v] = np.median(variables[v])
+            # collect mean values of variables
+            mean_dict = {}
+            for v in variables: mean_dict[v] = variables[v][0]
 
-        # generate lists sorted by mean variable importance
-        var = []
-        val = []
-        mean = []
-        std = []
-        i = 0
-        maxvalue = 0
-        for v, m in sorted(mean_dict.iteritems(), key = lambda (k, vl): (vl, k)):
-            i += 1
-            val.append(i)
-            var.append(v)
-            mean.append(m)
-            std.append( np.std(variables[v]) )
-            print(v,m)
-            if mean[-1]+std[-1] > maxvalue: maxvalue = mean[-1]+std[-1]
+            # generate lists sorted by mean variable importance
+            var = []
+            val = []
+            mean = []
+            std = []
+            i = 0
+            maxvalue = 0
+            for v, m in sorted(mean_dict.iteritems(), key = lambda (k, vl): (vl, k)):
+                i += 1
+                val.append(i)
+                var.append(v)
+                mean.append(m)
+                std.append( variables_std[v][0] )
+                print(v,m)
+                if mean[-1]+std[-1] > maxvalue: maxvalue = mean[-1]+std[-1]
+
+        
+
+        else:           
+            # collect variables and their relative importance
+            variables = {}
+            for ranking in rankings:
+                csv = pd.read_csv(ranking, header = 0, sep = ",", names = ["variable", "weight_sum"])
+                sum_of_weights = csv["weight_sum"].sum()
+                for row in csv.iterrows():
+                    if not row[1][0] in variables: variables[row[1][0]] = []
+                    variables[row[1][0]].append(row[1][1]/sum_of_weights)
+
+
+            # collect mean values of variables
+            mean_dict = {}
+            for v in variables: mean_dict[v] = np.median(variables[v])
+
+            # generate lists sorted by mean variable importance
+            var = []
+            val = []
+            mean = []
+            std = []
+            i = 0
+            maxvalue = 0
+            for v, m in sorted(mean_dict.iteritems(), key = lambda (k, vl): (vl, k)):
+                i += 1
+                val.append(i)
+                var.append(v)
+                mean.append(m)
+                std.append( np.std(variables[v]) )
+                print(v,m)
+                if mean[-1]+std[-1] > maxvalue: maxvalue = mean[-1]+std[-1]
+
 
         sorted_variables[jtcat] = var
 
@@ -155,7 +212,7 @@ for jtcat in args:
             plt.title(jtcat)
             plt.xlabel("mean of sum of input weights (in percent)")
             plt.tight_layout()
-            outfile = opts.outdir+"/"+opts.weight_type+"_weight_sums_"+jtcat+".pdf"
+            outfile = opts.outdir+"/"+opts.filename+opts.weight_type+"_weight_sums.pdf"
             plt.savefig(outfile)
             plt.clf() 
             print("saved plot to {}".format(outfile))
