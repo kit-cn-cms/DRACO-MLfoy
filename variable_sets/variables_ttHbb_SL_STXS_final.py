2714b164983d82aafe90a5a9f3e0ee4412106f18
Sebastian Wieland <Sebastian.Wieland@cern.ch>
2020-05-18 08:49:49 +0200

    added final variablesets

---
+++ variable_sets/variables_ttHbb_SL_STXS_final.py
@@ -0,0 +1,53 @@
+variables = {}
+
+variables["ge4j_ge4t"] = [
+    "Reco_tHq_h_pt",
+    "Reco_JABDT_tHq_log_h_pt",
+    "Reco_JABDT_tHq_log_min_hdau1_pt_hdau2_pt",
+    "Reco_tHW_h_pt",
+    "Reco_tHW_h_dr",
+    "Reco_JABDT_tHW_log_h_pt",
+    "Reco_ttH_h_dr",
+    "Reco_ttH_h_m",
+    "Reco_JABDT_ttH_log_h_pt",
+    "Reco_JABDT_ttH_log_toplep_m",
+
+
+    "Evt_Pt_TaggedJetsAverage",
+    "Evt_Dr_TaggedJetsAverage",
+    "Evt_M_TaggedJetsAverage",
+    "Evt_Pt_minDrTaggedJets",
+    "Evt_Pt_JetsAverage",
+
+    # didn't pass validation
+    # "Reco_tHq_h_dr",
+    ]
+
+
+variables["ge4j_3t"] = [
+    "Reco_JABDT_tHW_log_h_m",
+    "Reco_tHq_h_pt",
+    "Reco_JABDT_tHq_log_h_m",
+    "Reco_JABDT_tHq_log_h_pt",
+    "Reco_JABDT_tHq_log_min_hdau1_pt_hdau2_pt",
+    "Reco_tHW_h_pt",
+    "Reco_tHW_h_dr",
+    "Reco_JABDT_tHW_log_h_pt",
+    "Reco_ttH_h_dr",
+
+    "Evt_Pt_JetsAverage",
+    "Evt_Pt_TaggedJetsAverage",
+    "Evt_Dr_TaggedJetsAverage",
+
+    "TaggedJet_Pt[0]",
+
+    # didn't pass validation
+    # "Evt_HT",
+    # "Evt_HT_jets",
+    # "Evt_HT_tags",
+    # "Evt_M_TaggedJetsAverage",
+    # "Reco_tHW_bestJABDToutput",
+    # "Reco_tHq_h_dr",
+    ]
+
+all_variables = list(set( [v for key in variables for v in variables[key] ] ))
---
+++ variable_sets/variables_ttHbb_SL_final.py
@@ -0,0 +1,76 @@
+variables = {}
+
+variables["ge4j_ge4t"] = [
+    "Evt_CSV_avg_tagged",
+    "Evt_CSV_avg",
+    "Evt_M_minDrLepTag",
+    "Evt_Pt_JetsAverage",
+    "Evt_Pt_minDrTaggedJets",
+    "Evt_Deta_TaggedJetsAverage",
+    "Evt_Deta_JetsAverage",
+    "N_Jets",
+    "Reco_JABDT_ttbar_Jet_CSV_whaddau2",
+    "Reco_ttbar_toplep_m",
+    "Reco_tHq_bestJABDToutput",
+    "Reco_JABDT_tHq_abs_ljet_eta",
+    "Reco_JABDT_tHq_Jet_CSV_hdau1",
+    "Reco_JABDT_tHW_Jet_CSV_btop",
+    "memDBp",
+
+    # failed 2D validation
+    # "Evt_M_JetsAverage",
+    # "Reco_ttbar_bestJABDToutput",
+    # "Reco_ttH_toplep_m",
+    # "Reco_ttH_bestJABDToutput",
+    # "Evt_blr_transformed",
+    # "Reco_JABDT_ttH_Jet_CSV_hdau2",
+    # "Reco_JABDT_ttbar_Jet_CSV_whaddau1",
+
+    # failed 2D validation with lnN
+    # "Reco_tHW_bestJABDToutput",
+    # "Reco_JABDT_tHW_top_pt__P__h_pt__P__wb_pt__DIV__Evt_HT__P__Evt_Pt_MET__P__Lep_Pt",
+    # "Reco_JABDT_tHW_Jet_CSV_whaddau1",
+    # "CSV[2]",
+    # "Evt_M_TaggedJetsAverage",
+
+    ]
+
+variables["ge4j_3t"] = [
+    "memDBp",
+    "Evt_Deta_JetsAverage",
+    "Evt_M_TaggedJetsAverage", 
+    "Evt_CSV_avg", 
+    "Evt_blr_transformed",
+    "N_Jets",
+    "Reco_ttbar_toplep_m",
+    "Reco_ttbar_whad_m",
+    "Reco_JABDT_ttH_Jet_CSV_btoplep",
+    "Reco_JABDT_tHW_log_wb_m",
+
+    # failed 2D validation
+    # "Evt_CSV_dev",
+    # "Evt_M_Total",
+    # "Evt_CSV_avg_tagged",
+    # "Reco_ttH_bestJABDToutput",
+    # "Reco_tHq_bestJABDToutput",
+    # "Reco_tHW_whad_dr",
+    # "Reco_tHW_bestJABDToutput",
+    # "Reco_JABDT_tHW_Jet_CSV_btop",
+    # "Reco_JABDT_tHW_Jet_CSV_whaddau1",
+    # "Reco_ttbar_bestJABDToutput",  
+    # "Reco_JABDT_ttbar_Jet_CSV_btophad",
+    # "Reco_JABDT_ttbar_Jet_CSV_btoplep",
+   
+    # failed 2D validation with lnN
+    # "Reco_JABDT_ttbar_Jet_CSV_whaddau1",
+    # "Evt_JetPt_over_JetE",
+    # "Evt_h1",
+    # "Evt_Pt_minDrTaggedJets",
+    # "Evt_HT_tags",
+    # "Evt_Deta_TaggedJetsAverage",
+    # "Evt_M_JetsAverage"
+
+
+    ]
+
+all_variables = list(set( [v for key in variables for v in variables[key] ] ))
