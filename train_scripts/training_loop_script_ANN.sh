a28444958af922d9cf09eb52aed98206ccff75fd
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-06-19 14:04:12 +0200

    Correct Setup for training 100 ANNs with [100]

--- train_scripts/training_loop_script_ANN.sh
+++ train_scripts/training_loop_script_ANN.sh
@@ -6,6 +6,6 @@ epochs=4000
 iteration=100
 
 for ((i=0; i<$iteration; i+=1)); do
-    python train_template_bnn.py -o $output1"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e "$epochs" -q
-    python train_template_bnn.py -o $output2"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e "$epochs"
+    python train_template.py -o $output1"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e "$epochs" -q
+    python train_template.py -o $output2"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e "$epochs"
 done
