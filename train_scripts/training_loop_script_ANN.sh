5de0ed98dd0aeaef6e1602f0d2deb405d941748c
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-06-19 10:09:22 +0200

    Setup for training 100 ANNs (with and without QT) with [100]
    - State of DNN.py equals the start of commit 67b6a15d81c0dc96b4099867578fab3057387da3

---
+++ train_scripts/training_loop_script_ANN.sh
@@ -0,0 +1,11 @@
+#ANN
+name=binary_crossentropy_Adam_v2
+output1=QT_ANN_v2_training_
+output2=ANN_v2_training_
+epochs=4000
+iteration=100
+
+for ((i=0; i<$iteration; i+=1)); do
+    python train_template_bnn.py -o $output1"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e "$epochs" -q
+    python train_template_bnn.py -o $output2"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e "$epochs"
+done
