15373867ddf90db8ba99d304a275d0eb64943d08
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-06-30 14:28:43 +0200

    Setup for training 100 ANNs for variable selection
    - train_dnn.py [9833fa0c8c6e4483ced9e9d28548255dfe766ac9]
    - DNN.py [cca05b958dbf8ee6571cb709e36bbb328db4f32a]

--- train_scripts/training_loop_script.sh
+++ train_scripts/training_loop_script.sh
@@ -1,52 +1,11 @@
 #!/bin/bash
 #TODO: Check whether necessary to delete best_epoch.csv!
-#_v3 kl_use_exact  = True but no fixed prior
-#_v4 kl_use_exact  = True AND fixed prior by setting non trainable
 
-
-name=BNN
-output1=Variational_QT_BNN_training_
-output2=Variational_BNN_training_
-
+name=binary_crossentropy_Adam_modified
+output1=ANN_training_
 epochs=4000
-
-# cd /home/ycung/Desktop/DRACO-MLfoy/train_scripts/
-# layers=("50" "100" "200" "250" "300" "50,50" "50,50,50")
-# for i in "${!layers[@]}"; do
-#     python train_template_bnn.py -o $output1"${layers[$i]}"_v4 -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e $epochs -q --layers ${layers[$i]}
-#     python train_template_bnn.py -o $output2"${layers[$i]}"_v4 -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e $epochs --layers ${layers[$i]}
-# done
-
-# name=BNN
-# output1=QT_BNN_training_
-# output2=BNN_training_
-# epochs=4000
 iteration=100
 
-# cd /home/ycung/Desktop/DRACO-MLfoy/train_scripts/
-python train_template_bnn.py -o $output2"51" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e $epochs
-
-for ((i=52; i<$iteration; i+=1)); do
-    python train_template_bnn.py -o $output1"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e $epochs -q
-    python train_template_bnn.py -o $output2"$i" -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -p --printroc --binary --signal ttH -e $epochs
+for ((i=0; i<$iteration; i+=1)); do
+    python train_dnn.py -o $output1"$i" -i /local/scratch/ssd/ycung/new_h5_files_2017 -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -P --binary --signal ttH -e $epochs
 done
-
-# #ANN
-# name=binary_crossentropy_Adam
-# output1=QT_ANN_training_
-# output2=ANN_training_
-# epochs=4000
-# iteration=200
-
-# #BNN
-# names=('BNN_L2_5050' 'BNN_L3_505050')
-# epochs=4000
-
-# cd /home/ycung/Desktop/DRACO-MLfoy/train_scripts/
-# # for name in "${names[@]}"; do
-# python train_template_bnn.py -o QT_BNN_BNN_L2_5050_training -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n BNN -p --printroc --log --binary --signal ttH -e "$epochs" -q --layers 50,50
-# python train_template_bnn.py -o BNN_L2_5050_training_ -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n BNN -p --printroc --binary --signal ttH -e "$epochs" --layers 50,50
-# python train_template_bnn.py -o QT_BNN_BNN_L3_505050_training -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n BNN -p --printroc --log --binary --signal ttH -e "$epochs" -q --layers 50,50,50
-# python train_template_bnn.py -o BNN_L3_505050_training_ -i /local/scratch/ssd/nshadskiy/2017_nominal -c ge4j_ge3t -v allVariables_2017_bnn -n BNN -p --printroc --binary --signal ttH -e "$epochs" --layers 50,50,50
-# # done
-
