9abc4d38a21602a08fd514f1a3f3fa5cd52b4cef
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-06-30 18:46:41 +0200

    Add -p

--- train_scripts/training_loop_script.sh
+++ train_scripts/training_loop_script.sh
@@ -7,5 +7,5 @@ epochs=4000
 iteration=100
 
 for ((i=0; i<$iteration; i+=1)); do
-    python train_dnn.py -o $output1"$i" -i /local/scratch/ssd/ycung/new_h5_files_2017 -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -P --binary --signal ttH -e $epochs
+    python train_dnn.py -o $output1"$i" -i /local/scratch/ssd/ycung/new_h5_files_2017 -c ge4j_ge3t -v allVariables_2017_bnn -n "$name" -P -p --binary --signal ttH -e $epochs
 done
