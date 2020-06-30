0cf9022f1841a618a29ceb877588008aedcd8726
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-06-30 19:00:38 +0200

    Change label name

--- train_scripts/train_dnn.py
+++ train_scripts/train_dnn.py
@@ -141,7 +141,7 @@ if options.doPlots():
             printROC    = options.doPrintROC(),
             nbins       = 20, #me
             bin_range   = bin_range,
-            name        = "ANN Vorhersage #mu", #me
+            name        = "Ausgabewert #mu", #me
             sigScale    = options.getSignalScale())
 
         if options.isAdversary():
