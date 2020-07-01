174d7e9758381451c36ff25ce913408f2198930a
Jakob Lenschen <jlenschen@ekplx24.physik.uni-karlsruhe.de>
2020-07-01 14:42:39 +0200

    more

---
+++ train_scripts/jakob_load.py
@@ -0,0 +1,173 @@
+# global imports
+import keras
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+import os
+import sys
+
+# option handler
+import optionHandler
+options = optionHandler.optionHandler(sys.argv)
+
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(filedir)
+sys.path.append(basedir)
+
+# import class for DNN training
+import DRACO_Frameworks.DNN.DNN as DNN
+import DRACO_Frameworks.DNN.data_frame as df
+
+options.initArguments()
+
+# load samples
+input_samples = df.InputSamples(options.getInputDirectory(), options.getActivatedSamples(), options.getTestPercentage(), options.getAddSampleSuffix())
+
+weight_expr = 'x.Weight_XS * x.Weight_CSV * x.Weight_GEN_nom * x.lumiWeight'
+# define all samples
+input_samples.addSample(options.getDefaultName("ttH")  , label = "ttH"  , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("ttmb") , label = "ttmb" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("ttbb") , label = "ttbb" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("tt2b") , label = "tt2b" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("ttb")  , label = "ttb"  , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("ttcc") , label = "ttcc" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("ttlf") , label = "ttlf" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("tHq") ,  label = "tHq" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("tHW") ,  label = "tHW" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("sig") ,  label = "sig" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#input_samples.addSample(options.getDefaultName("bkg") ,  label = "bkg" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#
+input_samples.addSample(options.getDefaultName("ttH_HTXS_0") ,  label = "ttH_HTXS_0" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+input_samples.addSample(options.getDefaultName("ttH_HTXS_1") ,  label = "ttH_HTXS_1" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+input_samples.addSample(options.getDefaultName("ttH_HTXS_2") ,  label = "ttH_HTXS_2" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+input_samples.addSample(options.getDefaultName("ttH_HTXS_3") ,  label = "ttH_HTXS_3" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+input_samples.addSample(options.getDefaultName("ttH_HTXS_4") ,  label = "ttH_HTXS_4" , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#
+# additional samples for adversary training
+#if options.isAdversary():
+#    input_samples.addSample(options.getAddSampleName("ttmb"), label = "ttmb"+options.getAddSampleSuffix(), normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#    input_samples.addSample(options.getAddSampleName("ttbb"), label = "ttbb"+options.getAddSampleSuffix(), normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#    input_samples.addSample(options.getAddSampleName("tt2b"), label = "tt2b"+options.getAddSampleSuffix(), normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+#    input_samples.addSample(options.getAddSampleName("ttb") , label = "ttb"+options.getAddSampleSuffix() , normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr )
+
+if options.isBinary():
+    input_samples.addBinaryLabel(options.getSignal(), options.getBinaryBkgTarget())
+
+if not options.isAdversary():
+    # initializing DNN training class
+    dnn = DNN.DNN(
+        save_path       = options.getOutputDir(),
+        input_samples   = input_samples,
+        category_name   = options.getCategory(),
+        train_variables = options.getTrainVariables(),
+        # number of epochs
+        train_epochs    = options.getTrainEpochs(),
+        # metrics for evaluation (c.f. KERAS metrics)
+        eval_metrics    = ["acc"],
+        # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+        test_percentage = options.getTestPercentage(),
+        # balance samples per epoch such that there amount of samples per category is roughly equal
+        balanceSamples  = options.doBalanceSamples(),
+        evenSel         = options.doEvenSelection(),
+        norm_variables  = options.doNormVariables())
+else:
+    import DRACO_Frameworks.DNN.CAN as CAN
+    # initializing CAN training class
+    dnn = CAN.CAN(
+        save_path       = options.getOutputDir(),
+        input_samples   = input_samples,
+        category_name   = options.getCategory(),
+        train_variables = options.getTrainVariables(),
+        # number of epochs
+        train_epochs    = options.getTrainEpochs(),
+        # metrics for evaluation (c.f. KERAS metrics)
+        eval_metrics    = ["acc"],
+        # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+        test_percentage = options.getTestPercentage(),
+        # balance samples per epoch such that there amount of samples per category is roughly equal
+        balanceSamples  = options.doBalanceSamples(),
+        evenSel         = options.doEvenSelection(),
+        norm_variables  = options.doNormVariables(),
+        addSampleSuffix = options.getAddSampleSuffix())
+
+
+
+
+# attempt to load model with DRACO
+inputDirectory = '/home/jlenschen/Documents/DRACO-MLfoy/workdir/knot_reading/' + 'big_net_big_ge4j_ge4t/'
+
+dnn.load_trained_model(inputDirectory)
+dnn.summary()
+
+
+
+
+
+# variation plots
+if options.doVariations():
+    dnn.get_variations(options.isBinary())
+
+# plotting
+if options.doPlots():
+    # plot the evaluation metrics
+    dnn.plot_metrics(privateWork = options.isPrivateWork())
+
+    if options.isBinary():
+        # plot output node
+        bin_range = options.getBinaryBinRange()
+        dnn.plot_binaryOutput(
+            log         = options.doLogPlots(),
+            privateWork = options.isPrivateWork(),
+            printROC    = options.doPrintROC(),
+            bin_range   = bin_range,
+            name        = options.getName(),
+            sigScale    = options.getSignalScale())
+        if options.isAdversary():
+            dnn.plot_ttbbKS_binary(
+                log                 = options.doLogPlots(),
+                signal_class        = options.getSignal(),
+                privateWork         = options.isPrivateWork())
+    else:
+        # plot the confusion matrix
+        dnn.plot_confusionMatrix(
+            privateWork = options.isPrivateWork(),
+            printROC    = options.doPrintROC())
+
+        # plot the output discriminators
+        dnn.plot_discriminators(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork(),
+            printROC            = options.doPrintROC(),
+            sigScale            = options.getSignalScale())
+
+        # plot the output nodes
+        dnn.plot_outputNodes(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork(),
+            printROC            = options.doPrintROC(),
+            sigScale            = options.getSignalScale())
+
+        # plot event yields
+        dnn.plot_eventYields(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork(),
+            sigScale            = options.getSignalScale())
+
+        # plot closure test
+        dnn.plot_closureTest(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork())
+
+        # plot ttbb KS test
+        if options.isAdversary():
+            dnn.plot_ttbbKS(
+                log                 = options.doLogPlots(),
+                signal_class        = options.getSignal(),
+                privateWork         = options.isPrivateWork())
+
+if options.doGradients():
+    dnn.get_gradients(options.isBinary())
--- train_scripts/net_configs.py
+++ train_scripts/net_configs.py
@@ -262,3 +262,220 @@ config_dict["adversary_binary_test"] = {
         "adversary_epochs":         1,
         "adversary_iterations":     1,
 }
+config_dict["jakob_test_5"] = {
+        "layers":                   [5,5],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "L2_Norm":                  1e-3,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_big"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_low_dropout"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.10,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_high_dropout"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.70,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_relu"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "relu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_tanh"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "tanh",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_linear"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "linear",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_sigmoid"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "sigmoid",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_exponential"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "exponential",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_l1_high"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "L1_Norm":                  1e-3,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_l2_high"] = {
+        "layers":                   [100,100],
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.30,
+        "L2_Norm":                  1e-3,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+config_dict["jakob_test_random_normal"] = {
+        "layers":                   [100,100],
+        "kernel_initializer":       'random_normal',
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.50,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_zeros"] = {
+        "layers":                   [100,100],
+        "kernel_initializer":      'zeros',
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.50,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_random_uniform"] = {
+        "layers":                   [100,100],
+        "kernel_initializer":       'random_uniform',
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.50,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_ones"] = {
+        "layers":                   [100,100],
+        "kernel_initializer":       'ones',
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.50,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_truncated_normal"] = {
+        "layers":                   [100,100],
+        "kernel_initializer":       'truncated_normal',
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.50,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_glorot_normal"] = {
+        "layers":                   [100,100],
+        "kernel_initializer":       'glorot_normal',
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.50,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
+
+config_dict["jakob_test_glorot_uniform"] = {
+        "layers":                   [100,100],
+        "kernel_initializer":       'glorot_uniform',
+        "bias_initializer":         'zeros',
+        "loss_function":            "categorical_crossentropy",
+        "Dropout":                  0.50,
+        "batch_size":               4096,
+        "optimizer":                optimizers.Adam(1e-3),
+        "activation_function":      "elu",
+        "output_activation":        "Sigmoid",
+        "earlystopping_percentage": 0.02,
+        "earlystopping_epochs":     100,
+}
