f287af39c53a3072c325407500f1b140f24edaaa
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-07-04 22:07:13 +0200

    Change sigma_prior to 3.

--- DRACO_Frameworks/DNN/BNN.py
+++ DRACO_Frameworks/DNN/BNN.py
@@ -314,7 +314,7 @@ class BNN():
             n = kernel_size + bias_size
             return tf.keras.Sequential([
                 layers.VariableLayer(n, dtype=dtype), #trainable = False
-                layers.DistributionLambda(lambda t: tfd.Independent(tfd.Normal(loc=t, scale=1.), reinterpreted_batch_ndims=1)), #[:n]#1e-5 + tf.math.softplus(c + t[n:])
+                layers.DistributionLambda(lambda t: tfd.Independent(tfd.Normal(loc=t, scale=3.), reinterpreted_batch_ndims=1)), #[:n]#1e-5 + tf.math.softplus(c + t[n:])
                 ])
 
         # define input layer
