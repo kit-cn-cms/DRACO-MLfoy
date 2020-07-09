47e3c55a43f141aa22803ba34ca027899904a268
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-07-09 10:33:26 +0200

    Setup Training modified_Var_1

---
+++ train_scripts/train_bnn_modified_Var.py
@@ -0,0 +1,134 @@
+# global imports
+# so that matplotlib can be used over ssh
+import matplotlib #me
+matplotlib.use('Agg') #me
+
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+import os
+import sys
+
+# option handler
+import optionHandler
+options = optionHandler.optionHandler(sys.argv)
+
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(filedir)
+sys.path.append(basedir)
+
+# import class for BNN training
+import DRACO_Frameworks.DNN.BNN_modified_Var as BNN
+import DRACO_Frameworks.DNN.data_frame as df
+
+options.initArguments()
+
+# load samples
+input_samples = df.InputSamples(options.getInputDirectory(), options.getActivatedSamples(), options.getTestPercentage())
+
+# define all samples
+input_samples.addSample(options.getDefaultName("ttH"), label = "ttH", normalization_weight = options.getNomWeight())
+input_samples.addSample(options.getDefaultName("tthf") , label = "tthf" , normalization_weight = options.getNomWeight())
+input_samples.addSample(options.getDefaultName("ttcc") , label = "ttcc" , normalization_weight = options.getNomWeight())
+input_samples.addSample(options.getDefaultName("ttlf") , label = "ttlf" , normalization_weight = options.getNomWeight())
+
+if options.isBinary():
+    input_samples.addBinaryLabel(options.getSignal(), options.getBinaryBkgTarget())
+
+# initializing BNN training class
+bnn = BNN.BNN(
+    save_path       = options.getOutputDir(),
+    input_samples   = input_samples,
+    category_name   = options.getCategory(),
+    train_variables = options.getTrainVariables(),
+    # number of epochs
+    train_epochs    = options.getTrainEpochs(),
+    # metrics for evaluation (c.f. KERAS metrics)
+    eval_metrics    = ["acc"],
+    # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+    test_percentage = options.getTestPercentage(),
+    # balance samples per epoch such that there amount of samples per category is roughly equal
+    balanceSamples  = options.doBalanceSamples(),
+    shuffle_seed    = 42,
+    evenSel         = options.doEvenSelection(),
+    norm_variables  = options.doNormVariables(),
+    qt_transformed_variables = options.doQTNormVariables(),
+    restore_fit_dir = options.getRestoreFitDir(),
+    sys_variation   = False,
+    gen_vars        = False)
+
+# build BNN model
+bnn.build_model(options.getNetConfig())
+
+# set custom weight initialization
+if options.doCustomInitialization():
+    bnn.custom_initialization(post_mean=0., post_mean_std=0.1, untransformed_post_std_mean=-3., untransformed_post_std_std=0.1, use_default_post_mean = False, use_default_post_std = False)    
+
+# perform the training
+bnn.train_model()
+
+# evalute the trained model
+bnn.eval_model()
+
+# save information
+bnn.save_model(sys.argv, filedir, options.getNetConfigName())
+
+# save and print variable ranking according to the input layer weights
+bnn.get_input_weights()
+
+# # save and print variable ranking according to all layer weights
+# bnn.get_weights()
+
+# plotting
+if options.doPlots():
+    # plot the evaluation metrics
+    bnn.plot_metrics(privateWork = options.isPrivateWork())
+
+    if options.isBinary():
+        # plot output node
+        bin_range = options.getBinaryBinRange()
+        #bin_range = [-0.5, 1.5]
+        bnn.plot_binaryOutput(
+            log         = options.doLogPlots(),
+            privateWork = options.isPrivateWork(),
+            printROC    = options.doPrintROC(),
+            nbins       = 20, #me
+            bin_range   = bin_range,
+            name        = "BNN Ausgabewert #mu", #me
+            sigScale    = options.getSignalScale())
+        
+       
+    else:
+        # plot the confusion matrix
+        bnn.plot_confusionMatrix(
+            privateWork = options.isPrivateWork(),
+            printROC    = options.doPrintROC())
+
+        # plot the output discriminators
+        bnn.plot_discriminators(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork(),
+            printROC            = options.doPrintROC(),
+            sigScale            = options.getSignalScale())
+
+        # plot the output nodes
+        bnn.plot_outputNodes(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork(),
+            printROC            = options.doPrintROC(),
+            sigScale            = options.getSignalScale())
+
+        # plot event yields
+        bnn.plot_eventYields(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork(),
+            sigScale            = options.getSignalScale())
+
+        # plot closure test
+        bnn.plot_closureTest(
+            log                 = options.doLogPlots(),
+            signal_class        = options.getSignal(),
+            privateWork         = options.isPrivateWork())
