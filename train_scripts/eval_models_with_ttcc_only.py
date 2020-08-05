6308c6d7e439498e350db958321200deef5eaacf
Yee-Ying Cung <ycung@ekplx92.ekp.kit.edu>
2020-08-05 20:06:52 +0200

    Add evaluation of BNNs

--- train_scripts/eval_models_with_ttcc_only.py
+++ train_scripts/eval_models_with_ttcc_only.py
@@ -1,4 +1,4 @@
-# python eval_ANN_with_ttcc_only.py -i "/local/scratch/ssd/ycung/new_h5_files_2017_v2" -o comparison_ANN_only_ttcc -c ge4j_ge3t -v new_variableset_25_v2 --binary -S ttH -q -P --restorefitdir /home/ycung/Desktop/DRACO-MLfoy_thesis/workdir/training_ANN_150N/QT_ANN_150N_training_0_ge4j_ge3t/fit_data.pck
+# python eval_models_with_ttcc_only.py -i "/local/scratch/ssd/ycung/new_h5_files_2017_v2" -o comparison_all_models_only_ttcc -c ge4j_ge3t -v new_variableset_25_v2 --binary -S ttH -q -P --restorefitdir /home/ycung/Desktop/DRACO-MLfoy_thesis/workdir/training_ANN_150N/QT_ANN_150N_training_0_ge4j_ge3t/fit_data.pck
 # global imports
 # so that matplotlib can be used over ssh
 import matplotlib #me
@@ -208,11 +208,95 @@ dnn = DNN.DNN(
     norm_variables  = options.doNormVariables(),
     qt_transformed_variables = not options.doQTNormVariables(), #changed
     restore_fit_dir = None) #changed
+
+bnnDP_qt = BNN_DP.BNN_Flipout(
+    save_path       = options.getOutputDir(),
+    input_samples   = input_samples_binary, #changed
+    category_name   = options.getCategory(),
+    train_variables = options.getTrainVariables(),
+    # number of epochs
+    train_epochs    = options.getTrainEpochs(),
+    # metrics for evaluation (c.f. KERAS metrics)
+    eval_metrics    = ["acc"],
+    # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+    test_percentage = options.getTestPercentage(),
+    # balance samples per epoch such that there amount of samples per category is roughly equal
+    balanceSamples  = options.doBalanceSamples(),
+    shuffle_seed    = 42,
+    evenSel         = options.doEvenSelection(),
+    norm_variables  = options.doNormVariables(),
+    qt_transformed_variables = options.doQTNormVariables(),
+    restore_fit_dir = options.getRestoreFitDir(),
+    sys_variation   = False,
+    gen_vars        = False)
+
+bnnDP = BNN_DP.BNN_Flipout(
+    save_path       = options.getOutputDir(),
+    input_samples   = input_samples_binary, #changed
+    category_name   = options.getCategory(),
+    train_variables = options.getTrainVariables(),
+    # number of epochs
+    train_epochs    = options.getTrainEpochs(),
+    # metrics for evaluation (c.f. KERAS metrics)
+    eval_metrics    = ["acc"],
+    # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+    test_percentage = options.getTestPercentage(),
+    # balance samples per epoch such that there amount of samples per category is roughly equal
+    balanceSamples  = options.doBalanceSamples(),
+    shuffle_seed    = 42,
+    evenSel         = options.doEvenSelection(),
+    norm_variables  = options.doNormVariables(),
+    qt_transformed_variables = not options.doQTNormVariables(), #changed
+    restore_fit_dir = None, #changed
+    sys_variation   = False,
+    gen_vars        = False)
+
+bnn_qt = BNN.BNN(
+    save_path       = options.getOutputDir(),
+    input_samples   = input_samples_binary, #changed
+    category_name   = options.getCategory(),
+    train_variables = options.getTrainVariables(),
+    # number of epochs
+    train_epochs    = options.getTrainEpochs(),
+    # metrics for evaluation (c.f. KERAS metrics)
+    eval_metrics    = ["acc"],
+    # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+    test_percentage = options.getTestPercentage(),
+    # balance samples per epoch such that there amount of samples per category is roughly equal
+    balanceSamples  = options.doBalanceSamples(),
+    shuffle_seed    = 42,
+    evenSel         = options.doEvenSelection(),
+    norm_variables  = options.doNormVariables(),
+    qt_transformed_variables = options.doQTNormVariables(),
+    restore_fit_dir = options.getRestoreFitDir(),
+    sys_variation   = False,
+    gen_vars        = False)
+
+bnn = BNN.BNN(
+    save_path       = options.getOutputDir(),
+    input_samples   = input_samples_binary, #changed
+    category_name   = options.getCategory(),
+    train_variables = options.getTrainVariables(),
+    # number of epochs
+    train_epochs    = options.getTrainEpochs(),
+    # metrics for evaluation (c.f. KERAS metrics)
+    eval_metrics    = ["acc"],
+    # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+    test_percentage = options.getTestPercentage(),
+    # balance samples per epoch such that there amount of samples per category is roughly equal
+    balanceSamples  = options.doBalanceSamples(),
+    shuffle_seed    = 42,
+    evenSel         = options.doEvenSelection(),
+    norm_variables  = options.doNormVariables(),
+    qt_transformed_variables = not options.doQTNormVariables(), #changed
+    restore_fit_dir = None, #changed
+    sys_variation   = False,
+    gen_vars        = False)
 ########################################################################################################################
 
 n_iterations = 100
 suffix = "_only_ttcc"
-suffix_label = "nur " + r"$\mathregular{t\overline{t}+cc}$"
+suffix_label = "nur " + r"$\mathregular{t\overline{t}+c\overline{c}}$"
 
 input_dir_1 = work_dir+"training_ANN_150N/ANN_150N_training"
 input_dir_2 = work_dir+"training_ANN_150N/QT_ANN_150N_training"
@@ -223,3 +307,86 @@ nn2_pred, nn2_pred_std = ann_calc_mean_std(model=dnn_qt, input_dir=input_dir_2, 
 
 # Comparison ANN 150
 plot_correlation_two_NNs(nn1_pred, nn2_pred, nn1_pred_std, nn2_pred_std, "(ANN)"+ r'$^{\rm{D}}_{1 \times 150}$', "(ANN)"+ r'$^{\rm{D,T}}_{1 \times 150}$', output_dir, "ANN_150N_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+# Comparison BNNs (default and modified)
+input_dir_1  = work_dir+"Var_BNN_training_50_ge4j_ge3t"
+input_dir_2  = work_dir+"Var_QT_BNN_training_50_ge4j_ge3t"
+input_dir_3  = work_dir+"Flipout_BNN_training_50_ge4j_ge3t"
+input_dir_4  = work_dir+"Flipout_QT_BNN_training_50_ge4j_ge3t"
+input_dir_5  = work_dir+"modified_Flipout_V1_BNN_training_50_ge4j_ge3t"
+input_dir_6  = work_dir+"modified_Flipout_V1_QT_BNN_training_50_ge4j_ge3t"
+input_dir_7  = work_dir+"modified_Flipout_V3_BNN_training_50_ge4j_ge3t"
+input_dir_8  = work_dir+"modified_Flipout_V3_QT_BNN_training_50_ge4j_ge3t"
+input_dir_9  = work_dir+"modified_Var_V1_BNN_training_50_ge4j_ge3t"
+input_dir_10 = work_dir+"modified_Var_V1_QT_BNN_training_50_ge4j_ge3t"
+input_dir_11 = work_dir+"modified_Var_V2_BNN_training_50_ge4j_ge3t"
+input_dir_12 = work_dir+"modified_Var_V2_QT_BNN_training_50_ge4j_ge3t"
+
+
+
+nn1_pred, nn1_pred_std, labels1 = bnn.load_trained_model(input_dir_1, n_iterations, options.getOutputDir())
+nn2_pred, nn2_pred_std, labels2 = bnn_qt.load_trained_model(input_dir_2, n_iterations, options.getOutputDir())
+nn3_pred, nn3_pred_std, labels3 = bnnDP.load_trained_model(input_dir_3, n_iterations, options.getOutputDir())
+nn4_pred, nn4_pred_std, labels4 = bnnDP_qt.load_trained_model(input_dir_4, n_iterations, options.getOutputDir())
+nn5_pred, nn5_pred_std, labels5 = bnnDP.load_trained_model(input_dir_5, n_iterations, options.getOutputDir())
+nn6_pred, nn6_pred_std, labels6 = bnnDP_qt.load_trained_model(input_dir_6, n_iterations, options.getOutputDir())
+nn7_pred, nn7_pred_std, labels7 = bnnDP.load_trained_model(input_dir_7, n_iterations, options.getOutputDir())
+nn8_pred, nn8_pred_std, labels8 = bnnDP_qt.load_trained_model(input_dir_8, n_iterations, options.getOutputDir())
+nn9_pred, nn9_pred_std, labels9 = bnn.load_trained_model(input_dir_9, n_iterations, options.getOutputDir())
+nn10_pred, nn10_pred_std, labels10 = bnn_qt.load_trained_model(input_dir_10, n_iterations, options.getOutputDir())
+nn11_pred, nn11_pred_std, labels11 = bnn.load_trained_model(input_dir_11, n_iterations, options.getOutputDir())
+nn12_pred, nn12_pred_std, labels12 = bnn_qt.load_trained_model(input_dir_12, n_iterations, options.getOutputDir())
+
+
+
+# Comparison with and without QT
+plot_correlation_two_NNs(nn1_pred, nn2_pred, nn1_pred_std, nn2_pred_std, "(BNN)" + r'$^{\rm{DV}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV,T}}_{1 \times 50}$', output_dir, "BNN_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn3_pred, nn4_pred, nn3_pred_std, nn4_pred_std, "(BNN)" + r'$^{\rm{DP}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DP,T}}_{1 \times 50}$', output_dir, "BNN_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn5_pred, nn6_pred, nn5_pred_std, nn6_pred_std, "(BNN)" + r'$^{\rm{DF-Mod\ 1}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1,T}}_{1 \times 50}$', output_dir, "modV1_BNN_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn7_pred, nn8_pred, nn7_pred_std, nn8_pred_std, "(BNN)" + r'$^{\rm{DF-Mod\ 2}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2,T}}_{1 \times 50}$', output_dir, "modV3_BNN_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn9_pred, nn10_pred, nn9_pred_std, nn10_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', output_dir, "modV1_BNN_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn11_pred, nn12_pred, nn11_pred_std, nn12_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ B,T}}_{1 \times 50}$', output_dir, "modV2_BNN_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+# Comparison Default Var with mod Var V1, mod Var V2, Flip, mod Flip V1, mod Flip V3 (with and without QT)
+plot_correlation_two_NNs(nn1_pred, nn9_pred, nn1_pred_std, nn9_pred_std, "(BNN)" + r'$^{\rm{DV}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', output_dir, "BNN_Var_modV1_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn1_pred, nn11_pred, nn1_pred_std, nn11_pred_std, "(BNN)" + r'$^{\rm{DV}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ B}}_{1 \times 50}$', output_dir, "BNN_Var_modV2_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn1_pred, nn3_pred, nn1_pred_std, nn3_pred_std, "(BNN)" + r'$^{\rm{DV}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DP}}_{1 \times 50}$', output_dir, "BNN_Var_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn1_pred, nn5_pred, nn1_pred_std, nn5_pred_std, "(BNN)" + r'$^{\rm{DV}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1}}_{1 \times 50}$', output_dir, "BNN_Var_Flip_modV1_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn1_pred, nn7_pred, nn1_pred_std, nn7_pred_std, "(BNN)" + r'$^{\rm{DV}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2}}_{1 \times 50}$', output_dir, "BNN_Var_Flip_modV2_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+plot_correlation_two_NNs(nn2_pred, nn10_pred, nn2_pred_std, nn10_pred_std, "(BNN)" + r'$^{\rm{DV,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', output_dir, "QT_BNN_Var_modV1_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn2_pred, nn12_pred, nn2_pred_std, nn12_pred_std, "(BNN)" + r'$^{\rm{DV,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ B,T}}_{1 \times 50}$', output_dir, "QT_BNN_Var_modV2_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn2_pred, nn4_pred, nn2_pred_std, nn4_pred_std, "(BNN)" + r'$^{\rm{DV,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DP,T}}_{1 \times 50}$', output_dir, "QT_BNN_Var_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn2_pred, nn6_pred, nn2_pred_std, nn6_pred_std, "(BNN)" + r'$^{\rm{DV,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1,T}}_{1 \times 50}$', output_dir, "QT_BNN_Var_Flip_modV1_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn2_pred, nn8_pred, nn2_pred_std, nn8_pred_std, "(BNN)" + r'$^{\rm{DV,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2,T}}_{1 \times 50}$', output_dir, "QT_BNN_Var_Flip_modV2_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+# Comparison Default Flip with mod Var V1, mod Var V2, mod Flip V1, mod Flip V3 (with and without QT)
+plot_correlation_two_NNs(nn3_pred, nn9_pred, nn3_pred_std, nn9_pred_std, "(BNN)" + r'$^{\rm{DP}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', output_dir, "BNN_Flip_modV1_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn3_pred, nn11_pred, nn3_pred_std, nn11_pred_std, "(BNN)" + r'$^{\rm{DP}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ B}}_{1 \times 50}$', output_dir, "BNN_Flip_modV2_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn3_pred, nn5_pred, nn3_pred_std, nn5_pred_std, "(BNN)" + r'$^{\rm{DP}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1}}_{1 \times 50}$', output_dir, "BNN_Flip_modV1_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn3_pred, nn7_pred, nn3_pred_std, nn7_pred_std, "(BNN)" + r'$^{\rm{DP}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2}}_{1 \times 50}$', output_dir, "BNN_Flip_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+plot_correlation_two_NNs(nn4_pred, nn10_pred, nn4_pred_std, nn10_pred_std, "(BNN)" + r'$^{\rm{DP,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', output_dir, "QT_BNN_Flip_modV1_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn4_pred, nn12_pred, nn4_pred_std, nn12_pred_std, "(BNN)" + r'$^{\rm{DP,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ B,T}}_{1 \times 50}$', output_dir, "QT_BNN_Flip_modV2_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn4_pred, nn6_pred, nn4_pred_std, nn6_pred_std, "(BNN)" + r'$^{\rm{DP,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1,T}}_{1 \times 50}$', output_dir, "QT_BNN_Flip_modV1_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn4_pred, nn8_pred, nn4_pred_std, nn8_pred_std, "(BNN)" + r'$^{\rm{DP,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2,T}}_{1 \times 50}$', output_dir, "QT_BNN_Flip_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+# Comparison mod Var V1, with mod Var V2, mod Flip V1, mod Flip V3 (with and without QT)
+plot_correlation_two_NNs(nn9_pred, nn11_pred, nn9_pred_std, nn11_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ B}}_{1 \times 50}$', output_dir, "BNN_modV1_modV2_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn9_pred, nn5_pred, nn9_pred_std, nn5_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1}}_{1 \times 50}$', output_dir, "BNN_modV1_Var_modV1_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn9_pred, nn7_pred, nn9_pred_std, nn7_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2}}_{1 \times 50}$', output_dir, "BNN_modV1_Var_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+plot_correlation_two_NNs(nn10_pred, nn12_pred, nn10_pred_std, nn12_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DV-Mod\ B,T}}_{1 \times 50}$', output_dir, "QT_BNN_modV1_modV2_Var_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn10_pred, nn6_pred, nn10_pred_std, nn6_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1,T}}_{1 \times 50}$', output_dir, "QT_BNN_modV1_Var_modV1_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn10_pred, nn8_pred, nn10_pred_std, nn8_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2,T}}_{1 \times 50}$', output_dir, "QT_BNN_modV1_Var_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+# Comparison mod Var V2, mod Flip V1, mod Flip V3 (with and without QT)
+plot_correlation_two_NNs(nn11_pred, nn5_pred, nn11_pred_std, nn5_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1}}_{1 \times 50}$', output_dir, "BNN_modV2_Var_modV1_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn11_pred, nn7_pred, nn11_pred_std, nn7_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2}}_{1 \times 50}$', output_dir, "BNN_modV2_Var_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+plot_correlation_two_NNs(nn12_pred, nn6_pred, nn12_pred_std, nn6_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 1,T}}_{1 \times 50}$', output_dir, "QT_BNN_modV2_Var_modV1_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn12_pred, nn8_pred, nn12_pred_std, nn8_pred_std, "(BNN)" + r'$^{\rm{DV-Mod\ A,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2,T}}_{1 \times 50}$', output_dir, "QT_BNN_modV2_Var_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+
+# Comparison mod Flip V1, mod Flip V3 (with and without QT)
+plot_correlation_two_NNs(nn5_pred, nn7_pred, nn5_pred_std, nn7_pred_std, "(BNN)" + r'$^{\rm{DF-Mod\ 1}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2}}_{1 \times 50}$', output_dir, "BNN_modV1_Flip_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
+plot_correlation_two_NNs(nn5_pred, nn8_pred, nn5_pred_std, nn8_pred_std, "(BNN)" + r'$^{\rm{DF-Mod\ 1,T}}_{1 \times 50}$', "(BNN)" + r'$^{\rm{DF-Mod\ 2,T}}_{1 \times 50}$', output_dir, "QT_BNN_modV1_Flip_modV3_Flip_comparison"+suffix, privateWork=options.isPrivateWork(), suffix_label=suffix_label)
